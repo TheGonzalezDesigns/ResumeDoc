5e21a2838350b95980c2ede5642496adb96a604e
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-08 02:00:06 -0700

    {"commit_message": "Update main.ts"} The nuanced commit message for the given git diff is "Update main.ts".

--- main.ts
+++ main.ts
@@ -6,15 +6,15 @@ import { Document } from "./local_modules/Document"
 
 async function main(): Promise<void> {
   console.time('mainExecution');
-  const professionalBackground = await summarize("./context/professional");
-  const jobProfile = await summarize("./context/jobs");
+  const summaries = await Promise.all([summarize("./context/professional"), summarize("./context/jobs")]);
+  const [ professionalBackground, jobProfile ]  = summaries;
   const contentType = cType(parseInt(process.argv[2]));
   const directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were Hugo Gonzalez.` 
-  const frameQuery = `Generate a ${contentType} for the job listing based on the professional background`;
+  const frameQuery = `Generate a ${contentType} for the job listing based on the professional background that makes me look a senior engineer in the field.`;
   const prompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
   const res = await query(prompt);
 
-  //console.info(`Prompt: ${prompt}`);
+  console.info(`Prompt: ${prompt}`);
   //console.info(`Response: ${res}`);
 
   const resumeFilePath = "~/Documents/Resumes/Tests/ProfessionalSummary.txt"
@@ -24,4 +24,3 @@ async function main(): Promise<void> {
   console.timeEnd('mainExecution');
 }
 main();
-
