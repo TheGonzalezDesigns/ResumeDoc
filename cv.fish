cc6fbee9db431036145a91a02f19ce8c95f9eb6e
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-08 03:19:26 -0700

    "Update cv.fish script, read clipboard content and write to profile.txt"

--- cv.fish
+++ cv.fish
@@ -1,6 +1,17 @@
 #!/usr/bin/env fish
 
 # Read clipboard content and write to file
-xclip -selection clipboard -o > resume.txt
+xclip -selection clipboard -o > ./context/jobs/profile.txt
 
-echo "Content has been copied to resume.txt"
+# echo "Content has been copied to resume.txt"
+
+bun run ./main.ts 0
+
+echo "ProfessionalSummary.txt: \n"
+cat ~/Documents/Resumes/Tests/ProfessionalSummary.txt
+echo -e "\n"
+echo "Coverletter.txt: \n"
+cat ~/Documents/Coverletters/Tests/Letter.txt
+echo -e "\n"
+echo "SkillList.txt: \n"
+cat ~/Documents/Resumes/Tests/SkillList.txt
