9c2b7d46d10a0ee0d4f8b606563730db2a9724a4
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 23:01:52 -0700

    Refactor code to include frameQuery and add console logs for prompt and response

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 import { summarize } from "./local_modules/summarize";
 import { frame } from "./local_modules/frame";
+import { query } from "./local_modules/query"
 
 async function main(): Promise<void> {
+  console.time('mainExecution');
   const professionalBackground = await summarize("./context/professional");
   const jobProfile = await summarize("./context/jobs");
   const contentType = "Professional Summary";
   const directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt.` 
-  const query = `Generate a ${contentType} for the job listing based on the professional background`;
-  const prompt = await frame(directive, professionalBackground, jobProfile, query);
-  console.log(prompt);
+  const frameQuery = `Generate a ${contentType} for the job listing based on the professional background`;
+  const prompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
+  const res = await query(prompt);
+
+  console.info(`Prompt: ${prompt}`);
+  console.info(`Response: ${res}`);
+
+  console.timeEnd('mainExecution');
+
 }
 main();
 
