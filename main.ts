d709c4f2331e101c78b47bbbcea6c90cc973c9ec
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-08 02:44:29 -0700

    Update main.ts by generating nuanced commit message  The commit message summarizes the changes made in the code. Update main.ts: Generate nuanced commit message

--- main.ts
+++ main.ts
@@ -8,18 +8,45 @@ async function main(): Promise<void> {
   console.time('mainExecution');
   const summaries = await Promise.all([summarize("./context/professional"), summarize("./context/jobs")]);
   const [ professionalBackground, jobProfile ]  = summaries;
-  const contentType = cType(parseInt(process.argv[2]));
-  const directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were Hugo Gonzalez.` 
-  const frameQuery = `Generate a ${contentType} for the job listing based on the professional background that makes me look a senior engineer in the field.`;
-  const prompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
-  const res = await query(prompt);
+  let contentType = cType(0);
+  
+  let directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were Hugo Gonzalez.` 
+  let frameQuery = `Generate a ${contentType} for the job listing based on the professional background that makes me look a senior engineer in the field.`;
+  
+  const resumePrompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
+  
+  contentType = cType(1);
+  
+  directive = `You are a cover letter expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were Hugo Gonzalez.` 
+  frameQuery = `Generate a ${contentType} for the job listing based on the professional background that makes me look a senior engineer in the field. Only provide the body of the letter, do not add a greeting nor closing.`;
+  
+  const coverletterPrompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
 
-  console.info(`Prompt: ${prompt}`);
+
+  contentType = cType(2);
+  
+  directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were Hugo Gonzalez.` 
+  frameQuery = `Generate a ${contentType} for the job listing based on the professional background that makes me look a senior engineer in the field. Please format your response in unordered latex bullet points`;
+  
+  const skillListPrompt = await frame(directive, professionalBackground, jobProfile, frameQuery);
+  
+
+  const [professionalSummary, coverletterContent, skillList ] = await Promise.all([query(resumePrompt), query(coverletterPrompt), query(skillListPrompt)]);
+
+  //console.info(`Prompt: ${prompt}`);
   //console.info(`Response: ${res}`);
 
   const resumeFilePath = "~/Documents/Resumes/Tests/ProfessionalSummary.txt"
-  const Resume = new Document(resumeFilePath)
-  Resume.writeFile(res);
+  const resume = new Document(resumeFilePath);
+  resume.writeFile(professionalSummary);
+
+  const skillListFilePath = "~/Documents/Resumes/Tests/SkillList.txt"
+  const skillListFile = new Document(skillListFilePath);
+  skillListFile.writeFile(skillList);
+
+  const coverletterFilePath = "~/Documents/Coverletters/Tests/Letter.txt";
+  const coverletter = new Document(coverletterFilePath);
+  coverletter.writeFile(coverletterContent);
 
   console.timeEnd('mainExecution');
 }
