9355984c5520cfec75d5623886aced731230d3e7
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 21:43:57 -0700

    Added import for 'frame' in main.ts and implemented prompt generation for a professional summary based on given context and query.

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 import { summarize } from "./local_modules/summarize";
+import { frame } from "./local_modules/frame";
 
 async function main(): Promise<void> {
   const summary = await summarize("./context/legacy/data.txt");
-  console.log(summary);
+  const contentType = "Professional Summary";
+  const directive = `You are a resume expert. You will use the given context to answer the given query. Do not halucinate, only use the facts given to you in this prompt.` 
+  const query = `Generate a ${contentType} for the job listing`;
+  const prompt = await frame(directive, summary, query);
+  console.log(prompt);
 }
-
 main();
+
