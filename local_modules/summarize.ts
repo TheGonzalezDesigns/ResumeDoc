6b16f26e8a120f33e2ffbb35b1a2813d72b04970
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-08 02:04:57 -0700

    Updated text summarization logic and removed commented code.

--- local_modules/summarize.ts
+++ local_modules/summarize.ts
@@ -6,10 +6,11 @@ import * as fs from "fs";
 export async function summarize(filepath: string): Promise<string> {
   // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
   filepath = `${filepath}/profile.txt`;
-  console.info(`Summarizing ${filepath}...\n\n`)
+  console.info(`Summarizing ${filepath}...\n`)
   const text = fs.readFileSync(filepath, "utf8");
   const model = new OpenAI({
     modelName: "gpt-3.5-turbo-16k-0613", 
+    //modelName: "gpt-4", 
     temperature: 0.5,
   });
   const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 4000 });
