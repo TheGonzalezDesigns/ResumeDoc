c44d035f2101a40c42cd3cb27381574248fc1cb3
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 22:07:47 -0700

    Refactored code for generating a nuanced commit message based on the given git diff

--- main.ts
+++ main.ts
@@ -2,11 +2,12 @@ import { summarize } from "./local_modules/summarize";
 import { frame } from "./local_modules/frame";
 
 async function main(): Promise<void> {
-  const summary = await summarize("./context/legacy/data.txt");
+  const professionalBackground = await summarize("./context/professional");
+  const jobProfile = await summarize("./context/jobs");
   const contentType = "Professional Summary";
-  const directive = `You are a resume expert. You will use the given context to answer the given query. Do not halucinate, only use the facts given to you in this prompt.` 
-  const query = `Generate a ${contentType} for the job listing`;
-  const prompt = await frame(directive, summary, query);
+  const directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt.` 
+  const query = `Generate a ${contentType} for the job listing based on the professional background`;
+  const prompt = await frame(directive, professionalBackground, jobProfile, query);
   console.log(prompt);
 }
 main();
