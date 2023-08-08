c5d34c1897431c37ac6d6259b401d0517221d15c
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 19:58:32 -0700

    Update file import path for legacy data file

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { summarize } from "./local_modules/summarize";
 
 async function main(): Promise<void> {
-  const summary = await summarize("./local_modules/test.txt");
+  const summary = await summarize("./context/legacy/data.txt");
   console.log(summary);
 }
 
