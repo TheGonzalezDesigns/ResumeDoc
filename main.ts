7065a962a742ec42232375c889faac6db148db45
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 18:54:17 -0700

    Refactor main.ts to include a function that uses the summarize module to generate a summary from a text file.

--- main.ts
+++ main.ts
@@ -1,34 +1,8 @@
-import { OpenAI } from "langchain/llms/openai";
+import { summarize } from "./local_modules/summarize";
 
-const llm = new OpenAI({
-  temperature: 0.9,
-  frequencyPenalty: 0.5,
-  presencePenalty: 0.5
-});
+async function main(): Promise<void> {
+  const summary = await summarize("./local_modules/test.txt");
+  console.log(summary);
+}
 
-// Create a function that takes in a string and returns a response from the OpenAI language model
-const talkToLlm = async (input: string) => await llm.predict(input);
-
-
-// Use the talkToLlm function to create a conversation loop
-const conversationLoop = async () => {
-  let input = '';
-
-  const readline = async (): Promise<string> => {
-      let input = prompt("Please enter your input: ");
-      return input ? input : "No response";
-  };
-
-  while (input !== 'exit') {
-    console.log('Say something:');
-    input = await readline();
-
-    if (input !== 'exit') {
-      const response = await talkToLlm(input);
-      console.log(response);
-    }
-  }
-};
-
-conversationLoop();
-
+main();
