632bc1c126df5408dd3d65fe457169ad93d32753
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-07 18:19:02 -0700

    Initial commit for new file: local_modules/summarize.ts

--- local_modules/summarize.ts
+++ local_modules/summarize.ts
@@ -3,16 +3,19 @@ import { loadSummarizationChain } from "langchain/chains";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import * as fs from "fs";
 
-// In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
-const text = fs.readFileSync("test.txt", "utf8");
-const model = new OpenAI({ temperature: .5 });
-const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-const docs = await textSplitter.createDocuments([text]);
+export async function summarize(filepath: string): Promise<string> {
+  // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
+  const text = fs.readFileSync(filepath, "utf8");
+  const model = new OpenAI({ temperature: .5 });
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
 
-// This convenience function creates a document chain prompted to summarize a set of documents.
-const chain = loadSummarizationChain(model, { type: "map_reduce" });
-const res = await chain.call({
-  input_documents: docs,
-});
-const { text: summary } = res
-console.log(summary);
+  // This convenience function creates a document chain prompted to summarize a set of documents.
+  const chain = loadSummarizationChain(model, { type: "map_reduce" });
+  const res = await chain.call({
+    input_documents: docs,
+  });
+  const { text: summary } = res
+  return summary;
+}
+
