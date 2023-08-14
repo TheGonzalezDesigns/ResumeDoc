7f38d24aa8299d6ba1269c5a05df9d2e768b739a
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-08-13 23:07:25 -0700

    The commit message for the given git diff could be:  "Refactor summarize function and improve code readability"

--- local_modules/summarize.ts
+++ local_modules/summarize.ts
@@ -6,27 +6,26 @@ import * as fs from "fs";
 export async function summarize(filepath: string): Promise<string> {
   // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
   filepath = `${filepath}/profile.txt`;
-  console.info(`Summarizing ${filepath}...\n`)
+  console.info(`Summarizing ${filepath}...\n`);
   const text = fs.readFileSync(filepath, "utf8");
   const model = new OpenAI({
-    modelName: "gpt-3.5-turbo-16k-0613", 
-    //modelName: "gpt-4", 
+    modelName: "gpt-3.5-turbo-16k-0613",
+    //modelName: "gpt-4",
     temperature: 0.5,
   });
   const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 4000 });
   const docs = await textSplitter.createDocuments([text]);
 
   const params = {
-    type: "map_reduce" as const, 
-    length: 2000 
-  }
+    type: "map_reduce" as const,
+    length: 2000,
+  };
 
   // This convenience function creates a document chain prompted to summarize a set of documents.
   const chain = loadSummarizationChain(model, params);
   const res = await chain.call({
     input_documents: docs,
   });
-  const { text: summary } = res
+  const { text: summary } = res;
   return summary;
 }
-
