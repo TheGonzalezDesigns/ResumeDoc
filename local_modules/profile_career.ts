f4b1ebf07826a06cda767c75274f06ae63099c82
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-12 00:56:47 -0700

    Refactor profile_career.ts for improved text splitting

--- local_modules/profile_career.ts
+++ local_modules/profile_career.ts
@@ -1,7 +1,4 @@
-import { OpenAI } from "langchain/llms/openai";
-import { loadSummarizationChain } from "langchain/chains";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as fs from "fs";
 import { extract, extraction } from "./extract";
 
 const profile_career_chunk = async (
