3b85855ac2898ba03d7b496a0aab4318c215930d
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-11 23:40:54 -0700

    Initial commit for new file: local_modules/profile_career.ts

---
+++ local_modules/profile_career.ts
@@ -0,0 +1,45 @@
+import { OpenAI } from "langchain/llms/openai";
+import { loadSummarizationChain } from "langchain/chains";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
+import { extract, extraction } from "./extract";
+
+const profile_career_chunk = async (
+  career_chunk: string
+): Promise<extraction> => {
+  const extraction = await extract(
+    [
+      "technical_skill_array",
+      "non_technical_skill_array",
+      "career_achievements_array",
+      "career_experience_array",
+      "career_projects_array",
+    ],
+    career_chunk
+  );
+  console.info(extraction);
+  return extraction;
+};
+
+export const profile_career = async (): Promise<string> => {
+  // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
+  const filepath = "./context/professional/profile.txt";
+  const text = await Bun.file(filepath).text();
+  console.info(`Summarizing ${filepath}...\n`);
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 4000 });
+  const docs = await textSplitter.createDocuments([text]);
+  //console.info("docs:", docs);
+  const segments = [...docs].map((doc) => doc.pageContent);
+  const profiled_career_chunks = await Promise.all(
+    segments.map((career_chunk) => profile_career_chunk(career_chunk))
+  );
+  const extractions = [...segments].map((segment: string, i: number) => {
+    const extraction = {
+      profiled_career_chunk: profiled_career_chunks[i],
+      segment: segment,
+    };
+    return extraction;
+  });
+  console.info("extractions:", extractions);
+  return "juice!";
+};
