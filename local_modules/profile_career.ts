4c07241586124030e6408ea99b33ff4e7560872d
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-13 08:57:11 -0700

    Restructure career history for resume and cover letters.

--- local_modules/profile_career.ts
+++ local_modules/profile_career.ts
@@ -17,25 +17,24 @@ const profile_career_chunk = async (
   return extraction;
 };
 
-export const profile_career = async (): Promise<extractions> => {
+export const profile_career = async (): Promise<void> => {
   // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
   const filepath = "./context/professional/profile.txt";
   const text = await Bun.file(filepath).text();
-  console.info(`Summarizing ${filepath}...\n`);
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 4000 });
+  console.info(`Accessing ${filepath}...\n`);
+  console.info(`Analyzing and restructuring your career history.`);
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
   const docs = await textSplitter.createDocuments([text]);
   //console.info("docs:", docs);
   const segments = [...docs].map((doc) => doc.pageContent);
   const profiled_career_chunks = await Promise.all(
     segments.map((career_chunk) => profile_career_chunk(career_chunk))
   );
-  const extractions = [...segments].map((segment: string, i: number) => {
-    const extraction = {
-      profiled_career_chunk: profiled_career_chunks[i],
-      segment: segment,
-    };
-    return extraction;
-  });
-  console.info("extractions:", extractions);
-  return profiled_career_chunks;
+  await Bun.write(
+    "./context/professional/career_chunks.json",
+    JSON.stringify(profiled_career_chunks)
+  );
+  console.info(
+    `Your career history can now be used to generate resumes and cover-letters.`
+  );
 };
