4a85012a071b5960e032720a61a6c9e425b73013
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-13 08:20:03 -0700

    Refactored careerChunks to use optimized code for matching required skills.

--- local_modules/fuzzy_match.ts
+++ local_modules/fuzzy_match.ts
@@ -2,55 +2,38 @@ type Chunk = {
   [key: string]: any;
 };
 
-const matchSkills = async (
-  value: any,
-  requiredSkill: string
-): Promise<boolean> => {
+const matchSkills = (value: any, requiredSkill: string): boolean => {
   if (typeof value === "string") {
     return value.toLowerCase().includes(requiredSkill.toLowerCase());
   }
 
   if (Array.isArray(value)) {
-    const results = await Promise.all(
-      value.map((element) => matchSkills(element, requiredSkill))
-    );
-    return results.some((res) => res);
+    return value.some((element) => matchSkills(element, requiredSkill));
   }
 
   if (typeof value === "object" && value !== null && value !== undefined) {
-    const results = await Promise.all(
-      Object.values(value).map((subValue) =>
-        matchSkills(subValue, requiredSkill)
-      )
+    return Object.values(value).some((subValue) =>
+      matchSkills(subValue, requiredSkill)
     );
-    return results.some((res) => res);
   }
 
   return false;
 };
 
-const searchCareerChunk = async (
+const searchCareerChunk = (
   careerChunk: Chunk,
   jobProfileKey: string
-): Promise<boolean> => {
-  const results = await Promise.all(
-    Object.values(careerChunk).map((chunkValue) =>
-      matchSkills(chunkValue, jobProfileKey)
-    )
+): boolean => {
+  return Object.values(careerChunk).some((chunkValue) =>
+    matchSkills(chunkValue, jobProfileKey)
   );
-  return results.some((res) => res);
 };
 
-export const getRelevantCareerChunks = async (
+export const getRelevantCareerChunks = (
   careerChunks: Chunk[],
   jobProfileKeys: string[]
-): Promise<Chunk[]> => {
-  const chunkResults = await Promise.all(
-    careerChunks.map((chunk) =>
-      Promise.all(jobProfileKeys.map((key) => searchCareerChunk(chunk, key)))
-    )
-  );
-  return careerChunks.filter((_, index) =>
-    chunkResults[index].some((res) => res)
+): Chunk[] => {
+  return careerChunks.filter((chunk) =>
+    jobProfileKeys.some((key) => searchCareerChunk(chunk, key))
   );
 };
