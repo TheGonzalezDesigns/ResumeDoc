26a0991571cbc7ee4d29ee8eabb98a96a2f9cb65
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-13 22:12:28 -0700

    Initial commit for new file: local_modules/content_type.ts

---
+++ local_modules/content_type.ts
@@ -0,0 +1,21 @@
+enum content {
+  professional_summary = 0,
+  job_application_coverLetter = 1,
+  technical_skill_list = 2,
+  career_query = 3,
+}
+
+export const validate_content_type = (content_type: content): string => {
+  switch (content_type) {
+    case content.professional_summary:
+      return "Professional Summary";
+    case content.job_application_coverLetter:
+      return "Job Application Cover Letter";
+    case content.technical_skill_list:
+      return "Technical Skill List";
+    case content.career_query:
+      return "Career query response";
+    default:
+      throw "No content_type selected.";
+  }
+};
