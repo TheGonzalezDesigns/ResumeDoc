ef2f52d629517031c39bec76223579396bbefc95
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-13 20:02:22 -0700

    Refactor frame function variables and format prompt template

--- local_modules/frame.ts
+++ local_modules/frame.ts
@@ -5,19 +5,19 @@ type profile = extraction | string;
 
 export async function frame(
   directive: string,
-  professionalBackground: profile,
-  jobProfile: profile,
+  professional_background: profile,
+  job_profile: profile,
   query: string
 ): Promise<string> {
   const template =
-    "Directive: {directive}\n Professional Background: {professionalBackground}\n Job profile: {jobProfile} \n Query: {query}";
+    "Directive: {directive}\n Professional Background: {professional_background}\n Job profile: {job_profile} \n Query: {query}";
 
   const promptTemplate = PromptTemplate.fromTemplate(template);
 
   const formattedPromptTemplate = await promptTemplate.format({
     directive,
-    professionalBackground,
-    jobProfile,
+    professional_background,
+    job_profile,
     query,
   });
 
