c96c67cfcd71b45d0f5f0e875aa638bd4341d98b
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 15:09:34 -0700

    Deleted ../../text_splitter.ts

--- text_splitter.ts
+++
@@ -1,8 +0,0 @@
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-
-export const split_text = async (text: string): Promise<string[]> => {
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
-  const docs = await textSplitter.createDocuments([text]);
-  const segments = [...docs].map((doc) => doc.pageContent);
-  return segments;
-};
