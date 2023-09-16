92934493ad431c18cbb81c1a0e39bb120dd03fff
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 02:13:57 -0700

    Refactor main function and template rendering

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import { validate_content_type as cType } from "./local_modules/content_type";
 import ejs from "ejs";
 import { notify } from "./local_modules/notify";
 import { extract_career_chunks } from "./local_modules/analyze_career";
+import { get_array } from "local_modules/reform_data";
+import { Document } from "local_modules/document";
 
 async function main(): Promise<void> {
   const legal_name = "Hugo_Gonzalez";
@@ -12,6 +14,58 @@ async function main(): Promise<void> {
   console.time("mainExecution");
 
   try {
+    /*
+    const test_content = `
+The future is here, the future is now
+It's time to embrace the accelerationist vow
+To move beyond the status quo
+And create a world that's brand new
+
+A world of progress and innovation
+Where technology is our salvation
+Where we can use our minds and hearts
+To create a better world, no matter what the odds
+
+A world of automation and AI
+Where robots do the work, so we can be free
+Where we can use our skills and knowledge
+To create a better future, no matter what the college
+
+A world of abundance and prosperity
+Where everyone has access to opportunity
+Where we can use our resources wisely
+To create a better future, no matter what the size
+
+A world of collaboration and connection
+Where everyone works together for a common mission
+Where we can use our creativity and passion
+To create a better future, no matter what the fashion
+
+The future is here, the future is now
+It's time to embrace the accelerationist vow
+To move beyond the status quo
+And create a world that's brand new.
+
+    `;
+    const _content = {
+      fileName: "test",
+    };
+    const _resume = new Document(
+      //`./testing/${legal_name}_Resume_${_content.fileName}.html`
+      `./src/html/resumes/${legal_name}_Resume_${_content.fileName}.html`
+    );
+
+    const _cover_letter = new Document(
+      //`./testing/${legal_name}_cover_letter_${_content.fileName}.html`
+      `./src/html/cover_letters/${legal_name}_cover_letter_${_content.fileName}.html`
+    );
+
+    await Promise.all([
+      _resume.save(test_content),
+      _cover_letter.save(test_content),
+    ]);
+    //throw "Debugging...";
+*/
     const job_profile = await profile_job();
     const career_profile = await extract_career_chunks(job_profile);
     console.info(career_profile);
@@ -70,7 +124,7 @@ async function main(): Promise<void> {
       return matches ? JSON.parse(`[${matches[1]}]`) : [];
     };
 
-    const extracted_skill_list = extractJSONArray(skill_list);
+    const extracted_skill_list = await get_array(skill_list);
 
     const content: Generated_content = {
       professional_summary,
@@ -84,25 +138,35 @@ async function main(): Promise<void> {
       throw Error("Incomplete Generation.");
     }
 
+    const resume_template = new Document("./templates/resume.ejs");
+
     // Populate the templates
-    const resume_content = ejs.render("./templates/resume.ejs", {
+    const resume_content = ejs.render(resume_template.load(), {
       professional_summary: content.professional_summary,
       skill_list: content.skill_list,
     });
 
-    const cover_letter_full_content = ejs.render(
-      "./templates/cover_letter.ejs",
-      {
-        cover_letter_content: content.cover_letter_content,
-      }
+    const cover_letter_template = new Document("./templates/cover_letter.ejs");
+
+    const cover_letter_full_content = ejs.render(cover_letter_template.load(), {
+      cover_letter_content: content.cover_letter_content,
+    });
+
+    console.info("resume_content:", resume_content);
+    console.info("cover_letter_full_content:", cover_letter_full_content);
+
+    const resume = new Document(
+      `./src/html/resumes/${legal_name}_Resume_${content.fileName}.html`
     );
 
-    const resume = `./src/html/resumes/${legal_name}_Resume_${content.fileName}.html`;
+    const cover_letter = new Document(
+      `./src/html/cover_letters/${legal_name}_cover_letter_${content.fileName}.html`
+    );
 
-    const cover_letter = `./src/html/cover_letters/${legal_name}__cover-_letter_${content.fileName}.html`;
-
-    await Bun.write(resume, resume_content);
-    await Bun.write(cover_letter, cover_letter_full_content);
+    await Promise.all([
+      resume.save(resume_content),
+      cover_letter.save(cover_letter_full_content),
+    ]);
     notify(`All content for ${content.fileName} is ready.`);
   } catch (error) {
     console.error("Error logs:", error);
@@ -111,4 +175,7 @@ async function main(): Promise<void> {
 
   console.timeEnd("mainExecution");
 }
+
+console.time("main");
 main();
+console.timeEnd("main");
