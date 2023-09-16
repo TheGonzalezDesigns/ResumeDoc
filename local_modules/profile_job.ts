412c026f57772fc6b6216088807baca90d2b16cd
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 04:33:25 -0700

    Generate commit message:  "Update profile_job.ts - Add code to extract structured information from given job post"

--- local_modules/profile_job.ts
+++ local_modules/profile_job.ts
@@ -1,8 +1,38 @@
 import { extract, extraction } from "./extract";
+import { query } from "./query";
 
 export const profile_job = async (): Promise<extraction> => {
   const filepath = "./context/jobs/profile.txt";
   const description = await Bun.file(filepath).text();
+  const prompt = `
+    Please extract structured information from the given job post. Ensure you capture ALL relevant details. Follow the steps below:
+
+    Step 1: Extract the job title.
+    Step 2: List ALL technical skills mentioned.
+    Step 3: Identify the company name.
+    Step 4: Summarize the main job responsibilities.
+    Step 5: Extract all non-technical requirements, including education and years of experience.
+    Step 6: Capture the salary range.
+    Step 7: Note any mentioned benefits. If 'benefits' are referenced but not detailed, indicate 'benefits: not specified'.
+    Step 8: Identify the job location.
+
+    The output should be in JSON format similar to this:
+    {
+      'job_title': 'Job Title Here',
+      'technical_skills': ['Skill 1', 'Skill 2'],
+      ...
+    }
+
+    Ensure thoroughness and accuracy in your extraction.
+  `;
+  const data = await query(prompt);
+
+  const clean = await query(
+    `Please fix the following JSON if it is invalid, only respond with the json and nothing else: ${data}`
+  );
+  const extraction = JSON.parse(clean);
+
+  /*
   const extraction = await extract(
     [
       "job_title_string",
@@ -16,5 +46,6 @@ export const profile_job = async (): Promise<extraction> => {
     ],
     description
   );
+  */
   return extraction;
 };
