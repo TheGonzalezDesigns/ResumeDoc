46c73f7d4e83eb552204b6b1c659f8dc8d5f745a
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 02:14:46 -0700

    Refactor cv.fish for better readability and relocation of files

--- cv.fish
+++ cv.fish
@@ -6,14 +6,16 @@ set jobProfile (xclip -selection clipboard -o)
 
 echo "$jobProfile" > ~/projects/ResumeDoc/context/jobs/profile.txt
 
-bun run ./main.ts
+echo "Profile: $jobProfile"
 
+bun run ./main.ts
+#bun run ./main.ts > run.log 2>&1
 # Directory paths
 set html_root "./src/html/"
 set pdf_root "./src/pdfs/"
 
 # Sub directories
-set sub_dirs "coverletters" "resumes"
+set sub_dirs "cover_letters" "resumes"
 
 # Iterate over each sub-directory
 for dir in $sub_dirs
@@ -29,7 +31,8 @@ for dir in $sub_dirs
     for html_file in $html_dir*.html
         # Derive the output PDF filename
         set pdf_file (basename $html_file .html).pdf
-
+        echo -e "\nConverting $html_file to $pdf_dir$pdf_file"
+        cat $html_file
         # Convert the HTML to PDF
         python3 html2pdf.py $html_file $pdf_dir$pdf_file > /dev/null
 
@@ -39,9 +42,13 @@ for dir in $sub_dirs
   end
 
 for file in ./src/pdfs/resumes/*.pdf
+    echo -e "\nRelocating resume: $file"
+    #cat $file
     mv $file /home/c43/Documents/Resumes/Polished/Tailored/
 end
 
-for file in ./src/pdfs/coverletters/*.pdf
+for file in ./src/pdfs/cover_letters/*.pdf
+    echo -e "\nRelocating letter: $file"
+    #cat $file
     mv $file /home/c43/Documents/Coverletters/Tailored/
   end
