fb1f84b4795f02447fc12861f0764099b4f82235
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-17 02:32:21 -0700

    Refactor chunk_analysis.ts for enhanced keyword matching functionality

--- local_modules/experimental/chunk_analysis.ts
+++ local_modules/experimental/chunk_analysis.ts
@@ -5,36 +5,47 @@ export type chunk_score = {
   score: number;
 };
 
-export function analyze_chunk(
-  chunk: string,
-  job_profile: extraction
-): chunk_score {
+// Create a Set for faster keyword lookups
+let keywordSet: Set<string> = new Set();
+let regexList: RegExp[] = [];
+
+function init_keywords(job_profile: extraction) {
   // Extract all the important keywords from the job profile
   const job_keywords = job_profile.technical_skills.concat(
     job_profile.non_technical_skills
   );
 
+  keywordSet = new Set(job_keywords);
+  regexList = Array.from(keywordSet).map(
+    (keyword) => new RegExp(`\\b${keyword}\\b`, "i")
+  );
+}
+
+export function analyze_chunk(
+  chunk: string,
+  job_profile: extraction
+): chunk_score {
+  // Initialize keywords and regex only if not initialized
+  if (keywordSet.size === 0) {
+    init_keywords(job_profile);
+  }
+
   let match_count = 0;
 
   // Count the number of keywords that appear in the chunk
-  for (const keyword of job_keywords) {
-    const regex = new RegExp(`\\b${keyword}\\b`, "i"); // Match whole word, case-insensitive
+  for (const regex of regexList) {
     if (regex.test(chunk)) {
       match_count++;
     }
   }
 
-  // For this example, the score is simply the count of matches.
-  // This can be enhanced with more sophisticated scoring mechanisms if desired.
-  const score = match_count;
-
   return {
     chunk: chunk,
-    score: score,
+    score: match_count,
   };
 }
 
-export function is_chunk_worthy(chunk_analysis: extraction) {
+export function is_chunk_worthy(chunk_analysis: chunk_score): boolean {
   const threshold = 0.7; // adjustable
   return chunk_analysis.score > threshold;
 }
