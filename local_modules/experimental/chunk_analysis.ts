3a4a776fb7dee4d1557b1a4d220f6c1558e27b23
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-17 02:32:35 -0700

    Refactor keyword initialization and matching process in `analyze_chunk` function

--- local_modules/experimental/chunk_analysis.ts
+++ local_modules/experimental/chunk_analysis.ts
@@ -5,20 +5,22 @@ export type chunk_score = {
   score: number;
 };
 
-// Create a Set for faster keyword lookups
-let keywordSet: Set<string> = new Set();
-let regexList: RegExp[] = [];
+let keywordSet: Set<RegExp> = new Set();
+let isInitialized = false;
 
 function init_keywords(job_profile: extraction) {
   // Extract all the important keywords from the job profile
-  const job_keywords = job_profile.technical_skills.concat(
-    job_profile.non_technical_skills
-  );
+  const job_keywords = [
+    ...job_profile.technical_skills,
+    ...job_profile.non_technical_skills,
+  ];
 
-  keywordSet = new Set(job_keywords);
-  regexList = Array.from(keywordSet).map(
-    (keyword) => new RegExp(`\\b${keyword}\\b`, "i")
-  );
+  // Convert each keyword into a regex and add to the set
+  for (const keyword of job_keywords) {
+    keywordSet.add(new RegExp(`\\b${keyword}\\b`, "i"));
+  }
+
+  isInitialized = true;
 }
 
 export function analyze_chunk(
@@ -26,14 +28,14 @@ export function analyze_chunk(
   job_profile: extraction
 ): chunk_score {
   // Initialize keywords and regex only if not initialized
-  if (keywordSet.size === 0) {
+  if (!isInitialized) {
     init_keywords(job_profile);
   }
 
   let match_count = 0;
 
   // Count the number of keywords that appear in the chunk
-  for (const regex of regexList) {
+  for (const regex of keywordSet) {
     if (regex.test(chunk)) {
       match_count++;
     }
