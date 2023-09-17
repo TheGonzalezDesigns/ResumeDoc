2f8acbe3b95714d3e6c1f95eda086ddb0f1f9a24
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-17 01:09:53 -0700

    Refined commit message: "Refactored code for career profile creation and analysis"

--- _main.ts
+++ _main.ts
@@ -1,10 +1,23 @@
-import { profile_job } from "./local_modules/profile_job";
+import { refine_career_data } from "./local_modules/experimental/refinement";
+import { adaptive_chunking } from "./local_modules/experimental/extract";
+import { profile_career_chunk } from "./local_modules/experimental/profile_career";
+import { analyze_chunk } from "./local_modules/experimental/chunk_analysis";
+import { meta_analysis } from "./local_modules/experimental/meta_analysis";
+import { analyze_career } from "./local_modules/experimental/analyze_career";
 import { frame } from "./local_modules/frame";
+import { query } from "./local_modules/query";
+import ejs from "ejs";
 import { notify } from "./local_modules/notify";
 import { Document } from "local_modules/document";
-import { refine_career_data } from "./local_modules/experimental/refinement";
-import { meta_analysis } from "./local_modules/experimental/meta_analysis";
-import { extract_career_chunks } from "./local_modules/analyze_career";
+import { profile_job } from "local_modules/profile_job";
+import { split_text } from "local_modules/experimental/text_splitter";
+
+type bug = Record<string, any> | string | string[];
+
+const debug = (data: bug) => {
+  console.info("Debugging:", data);
+  throw "\nDebugging...\n";
+};
 
 async function main(): Promise<void> {
   const legal_name = "Hugo_Gonzalez";
@@ -13,40 +26,78 @@ async function main(): Promise<void> {
 
   try {
     const job_profile = await profile_job();
+    // Step 1: Text Splitting and Initial Chunking
+    const filepath = "./context/professional/profile.txt";
+    const raw_career_data = await Bun.file(filepath).text();
+    const initial_chunks = await split_text(raw_career_data);
 
-    // Step 1: Data Extraction & Refinement
-    const raw_career_data = await extract_career_chunks(job_profile);
-    console.info(raw_career_data);
-    throw "bananas...";
-    const refined_data = await refine_career_data(raw_career_data, job_profile);
+    // Step 2: Adaptive Chunking and Refinement
+    const refined_chunks = [];
+    for (const chunk of initial_chunks) {
+      const adapted_chunks = await adaptive_chunking(chunk, job_profile);
+      debug(adapted_chunks);
+      const refined_data = await refine_career_data(
+        adapted_chunks,
+        job_profile
+      );
+      refined_chunks.push(...refined_data);
+    }
 
-    // Step 2: Analysis
+    debug(refined_chunks);
+    // Step 3: Career Profile Creation
+    const profile_chunks = [];
+    for (const refined_chunk of refined_chunks) {
+      const chunk_profile = await profile_career_chunk(refined_chunk);
+      profile_chunks.push(chunk_profile);
+    }
+
+    // Step 4: Chunk Analysis and Scoring
+    const scored_chunks = [];
+    for (const profile_chunk of profile_chunks) {
+      const score = await analyze_chunk(profile_chunk, job_profile);
+      scored_chunks.push({
+        chunk: profile_chunk,
+        score: score,
+      });
+    }
+
+    // Step 5: Meta Analysis
     const analysis_result = metaAnalysis(
-      refined_data,
+      scored_chunks,
       job_profile,
       raw_career_data
     );
 
-    // Step 3: Content Framing
-    // (Use the analysis_result as needed in your framing logic)
-    const [professional_summary, cover_letter_content, skill_list] =
-      await Promise.all([
-        frame(legal_name, refined_data, job_profile, "resume query here"),
-        frame(legal_name, refined_data, job_profile, "cover letter query here"),
-        frame(legal_name, refined_data, job_profile, "skill list query here"),
-      ]);
+    // Step 6: Career Profile Aggregation
+    const aggregated_career_profile = analyze_career(
+      job_profile,
+      scored_chunks
+    );
 
-    // Step 4: Template Rendering
-    const resume_template = new Document("./templates/resume.ejs");
-    const cover_letter_template = new Document("./templates/cover_letter.ejs");
+    // Step 7: Final Structuring and Framing
+    const resume_content = frame(
+      aggregated_career_profile,
+      job_profile,
+      "resume"
+    );
+    const cover_letter_content = frame(
+      aggregated_career_profile,
+      job_profile,
+      "cover_letter"
+    );
 
-    // ... (rest of the template rendering logic remains the same)
+    // Save the results
+    const resume = new Document(`./src/html/resumes/${legal_name}_Resume.html`);
+    const cover_letter = new Document(
+      `./src/html/cover_letters/${legal_name}_cover_letter.html`
+    );
 
     await Promise.all([
       resume.save(resume_content),
-      cover_letter.save(cover_letter_full_content),
+      cover_letter.save(cover_letter_content),
     ]);
-    notify(`All content for ${content.fileName} is ready.`);
+
+    notify(`All content for ${legal_name} is ready.`);
   } catch (error) {
     console.error("Error logs:", error);
     notify(`The Doctor is ill`);
