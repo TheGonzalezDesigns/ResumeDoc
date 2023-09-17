f33f5b567b2f714660e01b295d1472d77cb70d04
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 17:17:55 -0700

    Refactor fuzzy_match.ts for weighted keyword matching and contextual analysis.  - Rename the function `weightedKeywordMatching` to `weighted_keyword_matching`. - Replace the parameter `keywords` with `extracted_details` in `weighted_keyword_matching` function. - Replace the function `contextualAnalysis` with `contextual_analysis`. - Update the comments to reflect the changes made.  The updated code is as follows:  ```typescript import { extraction_keys } from "./profile_career";  // Define the weights for each category const category_weights: { [key: string]: number } = {   technical_skill_array: 1.5,   non_technical_skill_array: 1,   career_achievements_array: 1,   career_experience_array: 1,   career_projects_array: 1, };  /**  * Compute a matching score based on the given keywords and text.  * Each keyword is weighted based on its category.  *  * @param extracted_details - The details extracted from the career profile.  * @param text - The text to be analyzed.  * @returns A matching score.  */ export function weighted_keyword_matching(   extracted_details: extraction_keys,   text: string ): number {   let score = 0;    // Iterate over each category in the extracted details   for (const category in extracted_details) {     // Ensure the category is a valid key in our category_weights     if (category_weights.hasOwnProperty(category)) {       const weight =         category_weights[category as keyof typeof category_weights];        // Iterate over each keyword in the category       for (const keyword of extracted_details[         category as keyof extraction_keys       ]) {         if (text.includes(keyword)) {           score += weight;         }       }     }   }    return score; }  /**  * Compute a contextual analysis score based on the given keywords and text.  *  * @param extracted_details - The details extracted from the career profile.  * @param text - The text to be analyzed.  * @returns A contextual analysis score.  */ export function contextual_analysis(   extracted_details: extraction_keys,   text: string ): number {   // Additional context analysis can be added here.   return weighted_keyword_matching(extracted_details, text); } ```  Please let me know if you need any further assistance.

--- local_modules/experimental/fuzzy_match.ts
+++ local_modules/experimental/fuzzy_match.ts
@@ -1,27 +1,59 @@
-export function weightedKeywordMatching(
-  keywords: string[],
+import { extraction_keys } from "./profile_career";
+
+// Define the weights for each category
+const category_weights: { [key: string]: number } = {
+  technical_skill_array: 1.5,
+  non_technical_skill_array: 1,
+  career_achievements_array: 1,
+  career_experience_array: 1,
+  career_projects_array: 1,
+};
+
+/**
+ * Compute a matching score based on the given keywords and text.
+ * Each keyword is weighted based on its category.
+ *
+ * @param extracted_details - The details extracted from the career profile.
+ * @param text - The text to be analyzed.
+ * @returns A matching score.
+ */
+export function weighted_keyword_matching(
+  extracted_details: extraction_keys,
   text: string
 ): number {
-  const weights = {
-    technical_skill: 1.5,
-    non_technical_skill: 1,
-    career_experience: 1,
-    career_achievement: 1,
-    career_projects: 1,
-  };
-
   let score = 0;
 
-  for (const keyword of keywords) {
-    if (text.includes(keyword)) {
-      score += weights[keyword] || 1;
+  // Iterate over each category in the extracted details
+  for (const category in extracted_details) {
+    // Ensure the category is a valid key in our category_weights
+    if (category_weights.hasOwnProperty(category)) {
+      const weight =
+        category_weights[category as keyof typeof category_weights];
+      // Iterate over each keyword in the category
+      for (const keyword of extracted_details[
+        category as keyof extraction_keys
+      ]) {
+        if (text.includes(keyword)) {
+          score += weight;
+        }
+      }
     }
   }
 
   return score;
 }
 
-export function contextualAnalysis(keywords: string[], text: string): number {
+/**
+ * Compute a contextual analysis score based on the given keywords and text.
+ *
+ * @param extracted_details - The details extracted from the career profile.
+ * @param text - The text to be analyzed.
+ * @returns A contextual analysis score.
+ */
+export function contextual_analysis(
+  extracted_details: extraction_keys,
+  text: string
+): number {
   // Additional context analysis can be added here.
-  return weightedKeywordMatching(keywords, text);
+  return weighted_keyword_matching(extracted_details, text);
 }
