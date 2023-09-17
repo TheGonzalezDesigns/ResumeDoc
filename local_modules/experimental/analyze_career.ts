f4e8bfaceece5e6ac4ead7fadeefe096fa94ba1a
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-16 17:26:38 -0700

    Refactor analyze_career function to include a threshold for score comparison, and rename relevantSegments to relevant_segments for consistency.

--- local_modules/experimental/analyze_career.ts
+++ local_modules/experimental/analyze_career.ts
@@ -1,15 +1,37 @@
-import { weighted_keyword_matching, contextual_analysis } from "./fuzzy_match";
+import { contextual_analysis } from "./fuzzy_match";
 
-export function analyze_career(job_profile, career_history_segments) {
-  const keywords = job_profile.technical_skills; // extend this to include more from the job profile if needed
-  let relevantSegments = [];
+type job_profile_type = {
+  technical_skill_array: string[];
+  non_technical_skill_array: string[];
+  career_achievements_array: string[];
+  career_experience_array: string[];
+  career_projects_array: string[];
+};
+
+type career_history_segment_type = {
+  technical_skill_array: string[];
+  non_technical_skill_array: string[];
+  career_achievements_array: string[];
+  career_experience_array: string[];
+  career_projects_array: string[];
+};
+
+export function analyze_career(
+  job_profile: job_profile_type,
+  career_history_segments: career_history_segment_type[]
+): career_history_segment_type[] {
+  const relevant_segments: career_history_segment_type[] = [];
+  const threshold = 5; // This threshold can be adjusted
 
   for (const segment of career_history_segments) {
-    const score = contextual_analysis(keywords, segment);
-    if (score > 0) {
-      relevantSegments.push(segment);
+    const score = contextual_analysis(
+      job_profile.technical_skill_array,
+      segment.technical_skill_array.join(" ")
+    );
+    if (score > threshold) {
+      relevant_segments.push(segment);
     }
   }
 
-  return relevantSegments;
+  return relevant_segments;
 }
