6e2d84291f484ffc37af48212c9bd28339711fea
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 00:10:23 -0700

    Updated chunk_analysis.ts, added analysis initialization, chunk analysis, and chunk worthiness checks.

--- local_modules/experimental/chunk_analysis.ts
+++ local_modules/experimental/chunk_analysis.ts
@@ -1,10 +1,11 @@
 import { extraction } from "../extract";
-import { debug, log } from "./debug";
+
 export type chunk_score = {
   chunk: string;
   score: number;
 };
 
+// Initialize analysis with regex list and threshold
 export const initialize_analysis = (job_profile: extraction) => {
   // Extract all the important keywords from the job profile
   const tech_skills = job_profile.technical_skills ?? [];
@@ -13,23 +14,23 @@ export const initialize_analysis = (job_profile: extraction) => {
   let regexList: RegExp[] = [];
   let threshold: number;
   const job_keywords = [...tech_skills, ...non_tech_skills];
+
   // Convert each keyword into a regex and add to the list
   regexList = [...job_keywords].map(
     (keyword) => new RegExp(`\\b${keyword}\\b`, "i")
   );
+
   // Set threshold as a proportion of total keywords
   const threshold_coefficient = 0.15;
   threshold = threshold_coefficient * job_keywords.length;
-  log(
-    `${threshold} = ${threshold_coefficient} * ${job_keywords.length}`,
-    "threshold"
-  );
+
   return {
     regexList,
     threshold,
   };
 };
 
+// Analyze a chunk of text for keyword matches
 export const analyze_chunk = (
   chunk: string,
   regexList: RegExp[],
@@ -41,8 +42,7 @@ export const analyze_chunk = (
   for (const regex of regexList) {
     if (regex.test(chunk)) {
       match_count++;
-      log({ chunk, regex, match_count, threshold }, "analyze_chunk");
-      if (match_count >= threshold) break; // Early exit
+      if (match_count >= threshold) break; // Early exit if threshold is met
     }
   }
 
@@ -52,6 +52,7 @@ export const analyze_chunk = (
   };
 };
 
+// Check if a chunk is worthy based on its analysis
 export function is_chunk_worthy(
   chunk_analysis: chunk_score,
   threshold: number
