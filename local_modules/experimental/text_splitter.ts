79d588e4f43ae3e8eb22217a5a40ba9c7a0c6fa7
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 00:11:25 -0700

    Refactor text_splitter.ts for better performance with optimized chunk size and overlap Optimize text_splitter.ts for better performance by adjusting chunk size and overlap

--- local_modules/experimental/text_splitter.ts
+++ local_modules/experimental/text_splitter.ts
@@ -1,7 +1,14 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { log } from "./debug";
 
 export const split_text = async (text: string): Promise<string[]> => {
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
+  const chunkSize = text.length / 10;
+  const chunkOverlap = chunkSize / 10;
+  log({ chunkSize, textSize: text.length }, "chunkSize");
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize,
+    chunkOverlap,
+  });
   const docs = await textSplitter.createDocuments([text]);
   const segments = [...docs].map((doc) => doc.pageContent);
   return segments;
