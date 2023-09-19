d0580440ac604716fece1cb0b56dd6985a2d4384
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 00:14:12 -0700

    "Update text_splitter.ts: Improve text splitting algorithm"

--- local_modules/experimental/text_splitter.ts
+++ local_modules/experimental/text_splitter.ts
@@ -1,15 +1,20 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { log } from "./debug";
 
+// Split text into segments
 export const split_text = async (text: string): Promise<string[]> => {
+  // Calculate the chunk size and overlap
   const chunkSize = text.length / 10;
   const chunkOverlap = chunkSize / 10;
-  log({ chunkSize, textSize: text.length }, "chunkSize");
+
+  // Create a text splitter instance with specified parameters
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize,
     chunkOverlap,
   });
+
+  // Create documents and extract page content
   const docs = await textSplitter.createDocuments([text]);
   const segments = [...docs].map((doc) => doc.pageContent);
+
   return segments;
 };
