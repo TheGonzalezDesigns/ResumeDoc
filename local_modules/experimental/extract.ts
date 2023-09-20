62bc2ff7073e06e5608bf325210a5df2a0886408
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 21:58:56 -0700

    Refactor `extract_main_categories` function to prompt the user to identify the main career categories from the given text. The prompt should instruct the user to list the categories as strings in a JSON array format. The text to be analyzed is: "Given the following text, identify the main career categories and list them as strings in a JSON array format. Avoid returning structured objects or nested arrays. Text: <text>".

--- local_modules/experimental/extract.ts
+++ local_modules/experimental/extract.ts
@@ -1,14 +1,40 @@
 import { query } from "../query";
 import { split_text } from "./text_splitter";
 import { analyze_chunk } from "./chunk_analysis";
+import { log } from "./debug";
 
 // Function to extract main categories from text
 export async function extract_main_categories(text: string): Promise<string[]> {
-  // Create a prompt for the main category extraction
-  const prompt = `Identify the main career categories from the following text: "${text}"`;
+  // Create a more explicit prompt for the main category extraction with an instruction for JSON format
+  const prompt = `Given the following text, identify the main career categories and list them as strings in a JSON array format. Avoid returning structured objects or nested arrays. Text: "${text}"`;
 
-  // Execute the query and return the result as an array
-  return [await query(prompt)];
+  let data = await query(prompt);
+  let parsedResult: string[] = [];
+
+  while (true) {
+    try {
+      // Attempt to parse the response
+      parsedResult = JSON.parse(data);
+
+      // Check if the parsed result is an array of strings
+      if (
+        !Array.isArray(parsedResult) ||
+        !parsedResult.every((item) => typeof item === "string")
+      ) {
+        throw new Error("Result is not an array of strings");
+      }
+
+      // If successful and it's an array of strings, break out of the loop
+      break;
+    } catch (err) {
+      // If there's an error (either parsing or not an array of strings), prompt for correction
+      data = await query(
+        `Please fix the following JSON ensuring it's a valid array of strings. Only respond with the json array and nothing else: ${data}`
+      );
+    }
+  }
+
+  return parsedResult;
 }
 
 // Function to extract details related to a category from text
@@ -16,11 +42,34 @@ export async function extract_details(
   category: string,
   text: string
 ): Promise<string[]> {
-  // Create a prompt for the category-specific details extraction
-  const prompt = `Extract details related to the category "${category}" from the following text: "${text}"`;
+  // Create a prompt for the category-specific details extraction with an instruction for JSON format
+  const prompt = `Given the category "${category}" and the text "${text}", extract relevant details about the category. Format the response as a valid Typescript String[]. Only respond with the array and nothing else.`;
 
-  // Execute the query and return the result as an array
-  return [await query(prompt)];
+  let data = await query(prompt);
+  let parsedResult: string[] = [];
+  //log(data, `/pre-parsed//:extracting ${category}`);
+
+  while (true) {
+    try {
+      // Attempt to parse the response
+      parsedResult = JSON.parse(data);
+
+      // Check if the parsed result is an array
+      if (!Array.isArray(parsedResult)) {
+        throw new Error("Result is not an array");
+      }
+
+      // If successful and it's an array, break out of the loop
+      break;
+    } catch (err) {
+      // If there's an error (either parsing or not an array), prompt for correction
+      data = await query(
+        `Please fix the following JSON ensuring it's a valid array. Only respond with the json array and nothing else: ${data}`
+      );
+    }
+  }
+  //log(parsedResult, `/parsed//:extracting ${category}`);
+  return parsedResult;
 }
 
 // Function for adaptive chunking of text
