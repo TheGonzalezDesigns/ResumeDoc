ed9fe98f68951a62b75b6238969f97b2377e02d1
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 22:44:37 -0700

    Initial commit for new file: local_modules/experimental/inversion.ts

---
+++ local_modules/experimental/inversion.ts
@@ -0,0 +1,24 @@
+import { query } from "./query";
+
+/**
+ * Inverts the provided text based on the given keywords.
+ * @param text The input text.
+ * @param keywords The keywords to focus on.
+ * @returns The inverted text.
+ */
+export async function keyword_inversion(
+  text: string,
+  keywords: string[]
+): Promise<string> {
+  // Create a prompt for the model
+  const prompt = `
+    Given the text "${text}", construct a concise sentence that prominently features the keywords ${keywords.join(
+    ", "
+  )} while excluding non-relevant parts.
+  `;
+
+  // Get the response from the model
+  const response = await query(prompt);
+
+  return response;
+}
