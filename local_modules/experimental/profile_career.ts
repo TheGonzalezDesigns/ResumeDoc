0c3a824b97836d9d24b908c36665460e1746095c
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-19 21:59:04 -0700

    Refactor profile_career_chunk for parallel extraction of category details

--- local_modules/experimental/profile_career.ts
+++ local_modules/experimental/profile_career.ts
@@ -1,16 +1,30 @@
 import { extract_main_categories, extract_details } from "./extract";
-import { extraction_keys } from "../extract";
+import { extraction_keys, meta } from "../extract";
+import { debug, log } from "../experimental/debug";
 
 export async function profile_career_chunk(
   career_chunk: string
-): Promise<extraction_keys> {
-  const mainCategories = await extract_main_categories(career_chunk);
-
+): Promise<meta> {
+  const main_categories = await extract_main_categories(career_chunk);
+  //log(main_categories, `main_categories`);
+  // Create an empty extraction object
   const extraction: extraction_keys = {};
 
-  for (const category of mainCategories) {
-    extraction[category] = await extract_details(category, career_chunk);
-  }
+  // Use Promise.all to extract details for each category in parallel
+  await Promise.all(
+    main_categories.map(async (category) => {
+      extraction[category] = await extract_details(category, career_chunk);
+      log(
+        { category, extraction: extraction[category] },
+        `profiling ${category}`
+      );
+    })
+  );
 
-  return extraction;
+  const meta_extraction: meta = {
+    raw_data: career_chunk,
+    categories: extraction,
+  };
+
+  return meta_extraction;
 }
