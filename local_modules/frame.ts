54060f170bc4b8be0ee19eb2530cc68571034f40
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 22:49:57 -0700

    Refactored Prompt: "Task: As a specialist, respond to the query based on the information provided. Stick to the facts presented and answer as if you are legal_name. Experience: professional_background Role: job_profile Question: query"

--- local_modules/frame.ts
+++ local_modules/frame.ts
@@ -1,30 +1,17 @@
-import { PromptTemplate } from "langchain/prompts";
 import { extraction } from "./extract";
 
 type profile = extraction | string;
 
-export async function frame(
+export const frame = (
   legal_name: string,
   professional_background: profile,
   job_profile: profile,
-  query: string
-): Promise<string> {
-  const directive = `You are a resume expert. You will use the given context to answer the given query. Do not hallucinate, only use the facts given to you in this prompt. Answer in the first person as if you were ${legal_name}.`;
+  query: string,
+  content_type: string
+): string => {
+  const directive = `As a ${content_type} specialist, respond to the query based on the information provided. Stick to the facts presented and answer as if you are ${legal_name}.`;
+  if (typeof job_profile !== "string")
+    job_profile = JSON.stringify(job_profile);
 
-  professional_background = JSON.stringify(professional_background);
-  job_profile = JSON.stringify(job_profile);
-
-  const template =
-    "Directive: {directive}\n Professional Background: {professional_background}\n Job profile: {job_profile} \n Query: {query}";
-
-  const prompt_template = PromptTemplate.fromTemplate(template);
-
-  const formatted_prompt_template = await prompt_template.format({
-    directive,
-    professional_background,
-    job_profile,
-    query,
-  });
-
-  return formatted_prompt_template;
-}
+  return `Refactored Prompt: "Task: ${directive}\nExperience: ${professional_background}\nRole: ${job_profile}\nQuestion: ${query}"`;
+};
