214a3881d73bde276415936f51061aee8b591371
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 22:22:15 -0700

    Refactor function to generate resume content, cover letter content, and skill list based on job and career profiles

--- main.ts
+++ main.ts
@@ -1,125 +1,120 @@
-import { profile_job } from "./local_modules/profile_job";
+import ejs from "ejs";
 import { frame } from "./local_modules/frame";
 import { query } from "./local_modules/query";
-import { validate_content_type as cType } from "./local_modules/content_type";
-import ejs from "ejs";
 import { notify } from "./local_modules/notify";
-import { get_array } from "local_modules/reform_data";
-import { Document } from "local_modules/document";
+import { Document } from "./local_modules/document";
+import { get_array } from "./local_modules/reform_data";
+import { profile_job } from "./local_modules/profile_job";
 import { summarize_career } from "./local_modules/summarize_career";
+import {
+  validate_content_type as cType,
+  content,
+} from "./local_modules/content_type";
 
+const stack = (
+  job_title: string,
+  content_type: string,
+  fine_tuned: string
+): string => {
+  const base = `Base-Prompt: "Craft a ${job_title} ${content_type} for the Job Listing Based on Your Expertise."`;
+  const instructions = fine_tuned.length
+    ? `Specific-Instructions: ${fine_tuned}`
+    : "";
+  const final = `${base} ${instructions}`;
+  return final;
+};
+
+/**
+ * The main function to generate a resume, cover letter, and skill list based on job and career profiles.
+ */
 async function main(): Promise<void> {
   const legal_name = "Hugo_Gonzalez";
   notify("The Doctor is ready");
   console.time("mainExecution");
 
   try {
+    // Fetch job and career profiles
     const job_profile = await profile_job();
     const career_profile = await summarize_career(job_profile);
+
+    // Log the career profile
     console.info(career_profile);
-    //throw "Debugging...";
-    let content_type = cType(0);
-
-    let frame_query = `Generate a ${content_type} for the job listing based on the professional background that makes me look a senior engineer in the field.`;
-
-    const resume_prompt = await frame(
-      legal_name,
-      career_profile,
-      job_profile,
-      frame_query
+    const job_title = job_profile.job_title;
+    const reframe = (content_type: content, prompt: string = "") =>
+      frame(
+        legal_name,
+        career_profile,
+        job_profile,
+        stack(job_title, cType(content_type), prompt),
+        cType(content_type)
+      );
+    // Generate resume content
+    const prompt_professional_summary = reframe(0);
+    const prompt_cover_letter = reframe(
+      1,
+      "Submit the main content of the letter only, excluding salutations or closings. Limit to 250 words, emphasizing suitability for the specified role"
+    );
+    const prompt_skill_list = reframe(
+      2,
+      "Provide an array of 10 or more skills relevant to the job. Each skill should clearly demonstrate your expertise. Format as a JavaScript array."
     );
 
-    content_type = cType(1);
-
-    frame_query = `Generate a ${content_type} for the job listing based on the professional background that makes me look a senior engineer in the field. Only provide the body of the letter, do not add a greeting nor closing. Keep it under 250 words while staying hyper-focused on making it seem like I'm the perfect candidate for the position given.`;
-
-    const cover_letter_prompt = await frame(
-      legal_name,
-      career_profile,
-      job_profile,
-      frame_query
-    );
-
-    content_type = cType(2);
-
-    frame_query = `Generate a ${content_type} for the job listing based on the professional background that makes me look a senior engineer in the field. Please format your response as an array of at least 10 skills. Each skill listed should be verbose and related to the job listing. Each skill should be impressive and prove I am a competent expert in the relevant field. Make sure the list is a javascript array`;
-
-    const skill_list_prompt = await frame(
-      legal_name,
-      career_profile,
-      job_profile,
-      frame_query
-    );
-
+    // Query to get final generated content
     const [professional_summary, cover_letter_content, skill_list] =
       await Promise.all([
-        query(resume_prompt),
-        query(cover_letter_prompt),
-        query(skill_list_prompt),
+        query(prompt_professional_summary),
+        query(prompt_cover_letter),
+        query(prompt_skill_list),
       ]);
 
+    // Define the generated content type
     interface Generated_content {
       professional_summary: string;
       skill_list: string[];
       cover_letter_content: string;
       fileName: string;
     }
-    const removeQuotes = (str: string) => str.replace(/['"]+/g, "");
-
-    const extractJSONArray = (str: string): string[] => {
-      const regex = /^\[(.*)\]$/s;
-      const matches = str.trim().match(regex);
-
-      return matches ? JSON.parse(`[${matches[1]}]`) : [];
-    };
 
     const extracted_skill_list = await get_array(skill_list);
 
+    // Populate the content object
     const content: Generated_content = {
       professional_summary,
       skill_list: extracted_skill_list,
       cover_letter_content,
       fileName: job_profile.company_name_string,
-    } as const;
+    };
 
-    if (content.skill_list.length < 5 || content.fileName == "") {
+    if (content.skill_list.length < 5 || content.fileName === "") {
       console.error("Malformed content:", content);
       throw Error("Incomplete Generation.");
     }
 
+    // Generate final documents
     const resume_template = new Document("./templates/resume.ejs");
-
-    // Populate the templates
-    const resume_content = ejs.render(resume_template.load(), {
-      professional_summary: content.professional_summary,
-      skill_list: content.skill_list,
-    });
+    const resume_content = ejs.render(resume_template.load(), content);
 
     const cover_letter_template = new Document("./templates/cover_letter.ejs");
-
     const cover_letter_full_content = ejs.render(cover_letter_template.load(), {
       cover_letter_content: content.cover_letter_content,
     });
 
-    console.info("resume_content:", resume_content);
-    console.info("cover_letter_full_content:", cover_letter_full_content);
-
+    // Save the generated documents
     const resume = new Document(
       `./src/html/resumes/${legal_name}_Resume_${content.fileName}.html`
     );
-
     const cover_letter = new Document(
       `./src/html/cover_letters/${legal_name}_cover_letter_${content.fileName}.html`
     );
-
     await Promise.all([
       resume.save(resume_content),
       cover_letter.save(cover_letter_full_content),
     ]);
+
     notify(`All content for ${content.fileName} is ready.`);
   } catch (error) {
     console.error("Error logs:", error);
-    notify(`The Doctor is ill`);
+    notify("The Doctor is ill");
   }
 
   console.timeEnd("mainExecution");
