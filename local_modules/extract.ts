2c308921224dac3b04d7521a07c938c6250f7954
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 18:28:02 -0700

    Refactored extract.ts to include new functions for extracting main categories, extracting category details, and adaptive chunking of text.

--- local_modules/extract.ts
+++ local_modules/extract.ts
@@ -1,57 +1,114 @@
-import { query } from "./query";
+import { query } from "../query";
+import { split_text } from "./text_splitter";
+import { analyze_chunk } from "./chunk_analysis";
+import { log } from "./debug";
 
-export type extraction = Record<string, any>;
-export type extractions = extraction[];
-export type extraction_keys = {
-  [key: string]: string[];
-};
-export type meta = {
-  raw_data: string;
-  categories: extraction_keys;
-};
+// Function to extract main categories from text
+export async function extract_main_categories(text: string): Promise<string[]> {
+  // Create a more explicit prompt for the main category extraction with an instruction for JSON format
+  const prompt = `Given the following text, identify the main career categories and list them as strings in a JSON array format. Avoid returning structured objects or nested arrays. Text: "${text}"`;
 
-export const extract = async (
-  extraction_args: string[],
-  data: string
-): Promise<extraction> => {
-  const type_description = "We are only concerned with strings for types.";
-  const format_instructions = "Format the response as a valid JSON object.";
-  const encoding_instructions =
-    "Encapsulate the response within a set of triple single quotation marks, e.g. ''' ... '''.";
-  const extraction_instructions = `Extract only data that is precisely related to [${[
-    ...extraction_args,
-  ].join(" | ")}].
-  `;
-  const promptTemplate = `
-    Your goal is to extract structured information from the user's input that matches the form described below.
-    When extracting information please make sure it matches the type information exactly.\n\n
-    Be extremely thorough in your extraction but do not add any attributes that do not appear in the schema shown below.\n\n
-    Type Description: ${type_description}\n\n
-    Format instructions: ${format_instructions}\n\n
-    Encoding instructions: ${encoding_instructions}\n\n
-    Extraction instructions: ${extraction_instructions}\n\n
-    Extract from:
-    \`\`\`
-    ${data}
-    \`\`\`
-    \n\n
-  `;
-  const reply = await query(promptTemplate);
-  const clean = await query(
-    `Please fix the following JSON if it is invalid, only respond with the json and nothing else: ${reply}`
-  );
-  /*
-  const response = `
-  ${promptTemplate}
-  -----------------------------------------------------------------------------------------------------------
-  \n\n
-  reply:
-  ${reply}
-  \n\n
-  clean:
-  ${clean}
-  `;
-  */
-  const final_extraction = JSON.parse(clean);
-  return final_extraction;
-};
+  let data = await query(prompt);
+  let parsedResult: string[] = [];
+
+  while (true) {
+    try {
+      // Attempt to parse the response
+      parsedResult = JSON.parse(data);
+
+      // Check if the parsed result is an array of strings
+      if (
+        !Array.isArray(parsedResult) ||
+        !parsedResult.every((item) => typeof item === "string")
+      ) {
+        throw new Error("Result is not an array of strings");
+      }
+
+      // If successful and it's an array of strings, break out of the loop
+      break;
+    } catch (err) {
+      // If there's an error (either parsing or not an array of strings), prompt for correction
+      data = await query(
+        `Please fix the following JSON ensuring it's a valid array of strings. Only respond with the json array and nothing else: ${data}`
+      );
+    }
+  }
+
+  return parsedResult;
+}
+
+// Function to extract details related to a category from text
+export async function extract_details(
+  category: string,
+  text: string
+): Promise<string[]> {
+  // Create a prompt for the category-specific details extraction with an instruction for JSON format
+  const prompt = `Given the category "${category}" and the text "${text}", extract relevant details about the category. Format the response as a valid Typescript String[]. Only respond with the array and nothing else.`;
+
+  let data = await query(prompt);
+  let parsedResult: string[] = [];
+  //log(data, `/pre-parsed//:extracting ${category}`);
+
+  while (true) {
+    try {
+      // Attempt to parse the response
+      parsedResult = JSON.parse(data);
+
+      // Check if the parsed result is an array
+      if (!Array.isArray(parsedResult)) {
+        throw new Error("Result is not an array");
+      }
+
+      // If successful and it's an array, break out of the loop
+      break;
+    } catch (err) {
+      // If there's an error (either parsing or not an array), prompt for correction
+      data = await query(
+        `Please fix the following JSON ensuring it's a valid array. Only respond with the json array and nothing else: ${data}`
+      );
+    }
+  }
+  //log(parsedResult, `/parsed//:extracting ${category}`);
+  return parsedResult;
+}
+
+// Function for adaptive chunking of text
+export async function adaptive_chunking(
+  text: string,
+  regex_list: RegExp[],
+  threshold: number
+): Promise<string[]> {
+  // Start by splitting the text into initial chunks
+  let chunks_to_analyze = await split_text(text);
+  const accepted_chunks: string[] = [];
+
+  // While there are chunks left to analyze
+  while (chunks_to_analyze.length > 0) {
+    // Bulk analyze all the current chunks
+    const chunk_analyses = await Promise.all(
+      chunks_to_analyze.map((chunk) =>
+        analyze_chunk(chunk, regex_list, threshold)
+      )
+    );
+
+    // Clear the current chunks for reevaluation
+    chunks_to_analyze = [];
+
+    // Process each chunk based on its analysis
+    for (let i = 0; i < chunk_analyses.length; i++) {
+      if (chunk_analyses[i].score >= threshold) {
+        // If the chunk meets the threshold, add it to accepted_chunks
+        accepted_chunks.push(chunk_analyses[i].chunk);
+      } else {
+        // If a chunk doesn't meet the threshold and is long enough, split it further for analysis
+        if (chunk_analyses[i].chunk.length > 100) {
+          const sub_chunks = await split_text(chunk_analyses[i].chunk);
+          chunks_to_analyze.push(...sub_chunks);
+        }
+      }
+    }
+  }
+
+  // Return the accepted chunks
+  return accepted_chunks;
+}
