18e1b7fffc494e0ccc5e970f58f5ebb22a0087b4
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 19:51:46 -0700

    Inverted text chunks based on keywords

--- local_modules/inversion.ts
+++ local_modules/inversion.ts
@@ -2,9 +2,10 @@ import { query } from "./query";
 
 /**
  * Inverts the provided text based on the given keywords.
- * @param text The input text.
- * @param keywords The keywords to focus on.
- * @returns The inverted text.
+ *
+ * @param {string} text - The input text to be inverted.
+ * @param {RegExp[]} keywords - The keywords to focus on while inverting.
+ * @returns {Promise<string>} A promise that resolves to the inverted text.
  */
 export const keyword_inversion = async (
   text: string,
@@ -12,21 +13,31 @@ export const keyword_inversion = async (
 ): Promise<string> => {
   // Create a prompt for the model
   const prompt = `
-  From the text "${text}".
+    From the text "${text}".
     Reconstruct the sentence(s) in the first person so that the text omits any topics or keywords that do not relate to the provided keywords: ${keywords.join(
       ", "
     )}
   `;
-  // Get the response from the model
+
+  // Query the model to get the response
   const response = await query(prompt);
 
   return response;
 };
 
+/**
+ * Inverts an array of text chunks based on the given keywords.
+ *
+ * @param {string[]} chunks - The array of text chunks to be inverted.
+ * @param {RegExp[]} keywords - The keywords to focus on while inverting.
+ * @returns {Promise<string[]>} A promise that resolves to an array of inverted text chunks.
+ */
 export const invert_chunks = async (
   chunks: string[],
   keywords: RegExp[]
-): Promise<string[]> =>
-  await Promise.all(
+): Promise<string[]> => {
+  // Map each chunk to its inverted version
+  return await Promise.all(
     chunks.map((chunk: string) => keyword_inversion(chunk, keywords))
   );
+};
