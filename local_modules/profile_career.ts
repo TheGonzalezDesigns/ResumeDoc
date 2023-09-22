f234fbfff05daed186bcfba133abdbb61aed1d39
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 18:34:52 -0700

    Refactored profile_career_chunk function to extract main categories and details for career_chunk

--- local_modules/profile_career.ts
+++ local_modules/profile_career.ts
@@ -1,40 +1,24 @@
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { extract, extraction, extractions } from "./extract";
+import { extract_main_categories, extract_details } from "./extract";
+import { extraction_keys, meta } from "../extract";
 
-const profile_career_chunk = async (
+export async function profile_career_chunk(
   career_chunk: string
-): Promise<extraction> => {
-  const extraction = await extract(
-    [
-      "technical_skill_array",
-      "non_technical_skill_array",
-      "career_achievements_array",
-      "career_experience_array",
-      "career_projects_array",
-    ],
-    career_chunk
-  );
-  return extraction;
-};
+): Promise<meta> {
+  const main_categories = await extract_main_categories(career_chunk);
+  // Create an empty extraction object
+  const extraction: extraction_keys = {};
 
-export const profile_career = async (): Promise<void> => {
-  // In this example, we use a `MapReduceDocumentsChain` specifically prompted to summarize a set of documents.
-  const filepath = "./context/professional/profile.txt";
-  const text = await Bun.file(filepath).text();
-  console.info(`Accessing ${filepath}...\n`);
-  console.info(`Analyzing and restructuring your career history.`);
-  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 2000 });
-  const docs = await textSplitter.createDocuments([text]);
-  //console.info("docs:", docs);
-  const segments = [...docs].map((doc) => doc.pageContent);
-  const profiled_career_chunks = await Promise.all(
-    segments.map((career_chunk) => profile_career_chunk(career_chunk))
+  // Use Promise.all to extract details for each category in parallel
+  await Promise.all(
+    main_categories.map(async (category) => {
+      extraction[category] = await extract_details(category, career_chunk);
+    })
   );
-  await Bun.write(
-    "./context/professional/career_chunks.json",
-    JSON.stringify(profiled_career_chunks)
-  );
-  console.info(
-    `Your career history can now be used to generate resumes and cover-letters.`
-  );
-};
+
+  const meta_extraction: meta = {
+    raw_data: career_chunk,
+    categories: extraction,
+  };
+
+  return meta_extraction;
+}
