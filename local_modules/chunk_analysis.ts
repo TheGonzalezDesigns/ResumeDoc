94b56b895cf02e4cd44129843e84c59f3b5b04e9
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 19:23:55 -0700

    Initialize chunk analysis with a job profile containing technical and non-technical skills. Analyze a chunk of text for keyword matches based on a given regex list and threshold.

--- local_modules/chunk_analysis.ts
+++ local_modules/chunk_analysis.ts
@@ -5,44 +5,58 @@ export type chunk_score = {
   score: number;
 };
 
-// Initialize analysis with regex list and threshold
+/**
+ * Initializes the analysis parameters based on the given job profile.
+ *
+ * @param {extraction} job_profile - The job profile containing technical and non-technical skills.
+ * @returns {Object} An object containing the regex list and threshold for chunk analysis.
+ */
 export const initialize_analysis = (job_profile: extraction) => {
-  // Extract all the important keywords from the job profile
+  // Extract all the important keywords from the job profile.
   const tech_skills = job_profile.technical_skills ?? [];
   const non_tech_skills = job_profile.non_technical_skills ?? [];
 
-  let regexList: RegExp[] = [];
+  let regex_list: RegExp[] = [];
   let threshold: number;
   const job_keywords = [...tech_skills, ...non_tech_skills];
 
-  // Convert each keyword into a regex and add to the list
-  regexList = [...job_keywords].map(
+  // Convert each keyword into a regex and add to the list.
+  regex_list = job_keywords.map(
     (keyword) => new RegExp(`\\b${keyword}\\b`, "i")
   );
 
-  // Set threshold as a proportion of total keywords
+  // Set threshold as a proportion of total keywords.
   const threshold_coefficient = 0.15;
   threshold = threshold_coefficient * job_keywords.length;
 
   return {
-    regexList,
-    threshold,
+    regexList: regex_list,
+    threshold: threshold,
   };
 };
 
-// Analyze a chunk of text for keyword matches
+/**
+ * Analyzes a chunk of text for keyword matches based on the given regex list and threshold.
+ *
+ * @param {string} chunk - The text chunk to be analyzed.
+ * @param {RegExp[]} regex_list - The list of regular expressions for keyword matching.
+ * @param {number} threshold - The minimum score needed for a chunk to be accepted.
+ * @returns {chunk_score} An object containing the chunk and its score.
+ */
 export const analyze_chunk = (
   chunk: string,
-  regexList: RegExp[],
+  regex_list: RegExp[],
   threshold: number
 ): chunk_score => {
   let match_count = 0;
 
-  // Count the number of keywords that appear in the chunk
-  for (const regex of regexList) {
+  // Count the number of keywords that appear in the chunk.
+  for (const regex of regex_list) {
     if (regex.test(chunk)) {
       match_count++;
-      if (match_count >= threshold) break; // Early exit if threshold is met
+
+      // Early exit if the threshold is met.
+      if (match_count >= threshold) break;
     }
   }
 
