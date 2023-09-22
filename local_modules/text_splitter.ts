3f9871844a17a3097c925977ab4bf2cae09b4103
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 19:21:21 -0700

    Refactor text splitting logic for optimization

--- local_modules/text_splitter.ts
+++ local_modules/text_splitter.ts
@@ -1,19 +1,24 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
-// Split text into segments
+/**
+ * Splits the given text into segments using the RecursiveCharacterTextSplitter from langchain.
+ *
+ * @param {string} text - The text to be split into segments.
+ * @returns {Promise<string[]>} A promise that resolves to an array of text segments.
+ */
 export const split_text = async (text: string): Promise<string[]> => {
-  // Calculate the chunk size and overlap
-  const chunkSize = text.length / 10;
-  const chunkOverlap = chunkSize / 10;
+  // Calculate the chunk size and overlap based on the length of the text.
+  const chunk_size = text.length / 10;
+  const chunk_overlap = chunk_size / 10;
 
-  // Create a text splitter instance with specified parameters
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize,
-    chunkOverlap,
+  // Create a text splitter instance with specified parameters.
+  const text_splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: chunk_size,
+    chunkOverlap: chunk_overlap,
   });
 
-  // Create documents and extract page content
-  const docs = await textSplitter.createDocuments([text]);
+  // Create documents and extract page content.
+  const docs = await text_splitter.createDocuments([text]);
   const segments = [...docs].map((doc) => doc.pageContent);
 
   return segments;
