0b613d0cb2ab5392210dafd6c4cdcde43b0dcb8f
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-21 18:52:44 -0700

    Refactor extract.ts  - Refactor the `extract_main_categories()` function to extract main categories from the given text. - Refactor the `extract_details()` function to extract details related to a specific category from the provided text. - Refactor the `adaptive_chunking()` function to perform adaptive chunking of the given text based on regular expressions and a threshold.

--- local_modules/extract.ts
+++ local_modules/extract.ts
@@ -1,22 +1,27 @@
-import { query } from "../query";
+import { query } from "./query";
 import { split_text } from "./text_splitter";
 import { analyze_chunk } from "./chunk_analysis";
-import { log } from "./debug";
 
-// Function to extract main categories from text
-export async function extract_main_categories(text: string): Promise<string[]> {
-  // Create a more explicit prompt for the main category extraction with an instruction for JSON format
+/**
+ * Extracts main categories from the given text.
+ * @param {string} text - The text to extract main categories from.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of main categories.
+ */
+export const extract_main_categories = async (
+  text: string
+): Promise<string[]> => {
+  // Construct a prompt for extracting main categories in JSON array format.
   const prompt = `Given the following text, identify the main career categories and list them as strings in a JSON array format. Avoid returning structured objects or nested arrays. Text: "${text}"`;
 
   let data = await query(prompt);
   let parsedResult: string[] = [];
 
+  // Loop to ensure the result is a valid array of strings.
   while (true) {
     try {
-      // Attempt to parse the response
-      parsedResult = JSON.parse(data);
+      parsedResult = JSON.parse(data); // Attempt to parse the response.
 
-      // Check if the parsed result is an array of strings
+      // Validate if parsed result is an array of strings.
       if (
         !Array.isArray(parsedResult) ||
         !parsedResult.every((item) => typeof item === "string")
@@ -24,10 +29,9 @@ export async function extract_main_categories(text: string): Promise<string[]> {
         throw new Error("Result is not an array of strings");
       }
 
-      // If successful and it's an array of strings, break out of the loop
-      break;
+      break; // Exit the loop if the result is a valid array of strings.
     } catch (err) {
-      // If there's an error (either parsing or not an array of strings), prompt for correction
+      // Prompt for correction if the result is not a valid array of strings.
       data = await query(
         `Please fix the following JSON ensuring it's a valid array of strings. Only respond with the json array and nothing else: ${data}`
       );
@@ -35,80 +39,83 @@ export async function extract_main_categories(text: string): Promise<string[]> {
   }
 
   return parsedResult;
-}
+};
 
-// Function to extract details related to a category from text
-export async function extract_details(
+/**
+ * Extracts details related to the given category from the provided text.
+ * @param {string} category - The category to extract details for.
+ * @param {string} text - The text to extract details from.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of details related to the category.
+ */
+export const extract_details = async (
   category: string,
   text: string
-): Promise<string[]> {
-  // Create a prompt for the category-specific details extraction with an instruction for JSON format
+): Promise<string[]> => {
+  // Construct a prompt for extracting category-specific details in array format.
   const prompt = `Given the category "${category}" and the text "${text}", extract relevant details about the category. Format the response as a valid Typescript String[]. Only respond with the array and nothing else.`;
 
   let data = await query(prompt);
   let parsedResult: string[] = [];
-  //log(data, `/pre-parsed//:extracting ${category}`);
 
+  // Loop to ensure the result is a valid array.
   while (true) {
     try {
-      // Attempt to parse the response
-      parsedResult = JSON.parse(data);
+      parsedResult = JSON.parse(data); // Attempt to parse the response.
 
-      // Check if the parsed result is an array
+      // Validate if parsed result is an array.
       if (!Array.isArray(parsedResult)) {
         throw new Error("Result is not an array");
       }
 
-      // If successful and it's an array, break out of the loop
-      break;
+      break; // Exit the loop if the result is a valid array.
     } catch (err) {
-      // If there's an error (either parsing or not an array), prompt for correction
+      // Prompt for correction if the result is not a valid array.
       data = await query(
         `Please fix the following JSON ensuring it's a valid array. Only respond with the json array and nothing else: ${data}`
       );
     }
   }
-  //log(parsedResult, `/parsed//:extracting ${category}`);
-  return parsedResult;
-}
 
-// Function for adaptive chunking of text
-export async function adaptive_chunking(
+  return parsedResult;
+};
+
+/**
+ * Performs adaptive chunking of the given text based on the provided regex list and threshold.
+ * @param {string} text - The text to be chunked.
+ * @param {RegExp[]} regex_list - The list of regular expressions to be used for chunk analysis.
+ * @param {number} threshold - The threshold to accept a chunk.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of accepted chunks.
+ */
+export const adaptive_chunking = async (
   text: string,
   regex_list: RegExp[],
   threshold: number
-): Promise<string[]> {
-  // Start by splitting the text into initial chunks
-  let chunks_to_analyze = await split_text(text);
+): Promise<string[]> => {
+  let chunks_to_analyze = await split_text(text); // Initial splitting of the text into chunks.
   const accepted_chunks: string[] = [];
 
-  // While there are chunks left to analyze
+  // Loop to analyze all the chunks.
   while (chunks_to_analyze.length > 0) {
-    // Bulk analyze all the current chunks
+    // Bulk analysis of the current chunks.
     const chunk_analyses = await Promise.all(
       chunks_to_analyze.map((chunk) =>
         analyze_chunk(chunk, regex_list, threshold)
       )
     );
+    chunks_to_analyze = []; // Clear the current chunks for reevaluation.
 
-    // Clear the current chunks for reevaluation
-    chunks_to_analyze = [];
-
-    // Process each chunk based on its analysis
-    for (let i = 0; i < chunk_analyses.length; i++) {
-      if (chunk_analyses[i].score >= threshold) {
-        // If the chunk meets the threshold, add it to accepted_chunks
-        accepted_chunks.push(chunk_analyses[i].chunk);
-      } else {
-        // If a chunk doesn't meet the threshold and is long enough, split it further for analysis
-        if (chunk_analyses[i].chunk.length > 100) {
-          const sub_chunks = await split_text(chunk_analyses[i].chunk);
-          chunks_to_analyze.push(...sub_chunks);
-        }
+    // Evaluate each chunk based on its analysis.
+    for (const analysis of chunk_analyses) {
+      if (analysis.score >= threshold) {
+        // Accept the chunk if it meets the threshold.
+        accepted_chunks.push(analysis.chunk);
+      } else if (analysis.chunk.length > 100) {
+        // Split the chunk further for analysis if it doesn't meet the threshold and is long enough.
+        const sub_chunks = await split_text(analysis.chunk);
+        chunks_to_analyze.push(...sub_chunks);
       }
     }
   }
 
-  // Return the accepted chunks
   return accepted_chunks;
-}
+};
