92cbc48059efe7fec856a919f5bdd5037906b90a
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-24 00:33:45 -0700

    Add job profile extraction functionality to `profile_job` function

--- local_modules/profile_job.ts
+++ local_modules/profile_job.ts
@@ -1,11 +1,33 @@
 import { extraction } from "./extract";
 import { query } from "./query";
 
+/**
+ * Profiles a job and extracts essential details based on the job description.
+ *
+ * @returns {Promise<extraction>} A promise that resolves to an extraction object containing detailed job profile information.
+ * @throws {Error} Throws an error if the extraction fails.
+ */
 export const profile_job = async (): Promise<extraction> => {
   const filepath = "./context/jobs/profile.txt";
   const description = await Bun.file(filepath).text();
   const prompt = `
     Given the job listing below, ensure a thorough and complete extraction of all details, especially focusing on capturing ALL technical skills, responsibilities, and non-technical requirements mentioned throughout the job description.
+
+    Model the response after this schema:
+    {
+      job_title: "",
+      technical_skills: [],
+      company_name: "",
+      job_responsibilities_summary: "",
+      non_technical_requirements: {
+        educational_qualifications: "",
+        years_of_experience: "",
+        specific_industry_experience: ""
+      },
+      salary_range: "",
+      benefits: [],
+      location: ""
+    }
 
     1. Extract the exact job title.
     2. List EVERY technical skill mentioned, wherever it appears in the job description.
@@ -16,7 +38,7 @@ export const profile_job = async (): Promise<extraction> => {
     7. If 'benefits' are mentioned but aren't detailed, indicate 'benefits: not specified'. Otherwise, list them.
     8. Determine the job's location.
 
-    Remember, accuracy, and completeness are paramount. Ensure the output is in a valid JSON format without any errors or formatting issues.
+    Remember, accuracy and completeness are paramount. Ensure the output is in a valid JSON format without any errors or formatting issues.
 
     Job Listing:
     ${description}
