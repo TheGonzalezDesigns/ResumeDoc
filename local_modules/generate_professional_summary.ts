1688f7fa61f449c1acf480fde0f1c4542e67720a
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-23 22:41:03 -0700

    Craft concise, clear, and engaging professional summary emphasizing achievements, unique contributions aligned with responsibilities, relevant experiences, alignment with company culture, and potential significant contribution. Avoid unnecessary details, redundancies, and jargon. Highlight proficiency in technical skills and how it can meet company goals.

--- local_modules/generate_professional_summary.ts
+++ local_modules/generate_professional_summary.ts
@@ -9,21 +9,23 @@ export const generate_professional_summary = async (
   const job_title = job_profile.job_title;
   const company_name = job_profile.company_name;
   const technical_skills = job_profile.technical_skills.join(", ");
-  const responsibilities = job_profile.job_responsibilities;
-  const non_technical_requirements = job_profile.non_technical_requirements;
+  const responsibilities = job_profile.job_responsibilities.join(", ");
+  const non_technical_requirements =
+    job_profile.non_technical_requirements.specific_industry_experience ||
+    "industry-specific experiences";
 
   // Craft the professional summary prompt dynamically
   const professional_summary_prompt = `
     Craft a concise, clear, and highly personalized professional summary using this profile of my career: ${career_summary}
 
     Strive to:
-    - Include specific and quantifiable achievements, using examples or case studies, demonstrating tangible impact made in previous roles, particularly those relevant to a ${job_title} role at ${company_name}.
-    - Highlight unique contributions, innovations, or approaches, ensuring they are directly aligned with the responsibilities and requirements of the ${job_title} role.
-    - Detail experiences and projects that are highly relevant to ${company_name}, using clear, concise, and straightforward language.
-    - Reflect on how the candidate’s approach, philosophy, or work ethic aligns with the culture and values of ${company_name}, providing insights to showcase understanding and fit.
-    - Conclude with a compelling and personalized statement, expressing genuine enthusiasm, alignment with the role at ${company_name}, and the potential significant contribution to be made.
+    - Include specific and quantifiable achievements, demonstrating tangible impact made in previous roles, with examples relevant to a ${job_title} role at ${company_name}.
+    - Emphasize unique contributions and innovations that are directly aligned with the responsibilities of ${responsibilities} and requirements of the ${job_title} role.
+    - Detail experiences and projects that demonstrate a deep understanding of ${non_technical_requirements} and are highly relevant to ${company_name}, using clear and concise language.
+    - Reflect on how the candidate’s approach, philosophy, or work ethic aligns with the culture and values of ${company_name}, providing specific insights or examples to showcase understanding and fit.
+    - Conclude with a compelling and personalized statement, expressing genuine enthusiasm and alignment with the role at ${company_name}, and detailing the potential significant contribution to be made.
 
-    Ensure the summary is eloquent, engaging, succinct, and highly relevant, avoiding any unnecessary details, redundancies, or jargon.
+    Ensure the summary is eloquent, engaging, succinct, and highly relevant, avoiding any unnecessary details, redundancies, or jargon. Highlight the proficiency in ${technical_skills} and how it can be leveraged to meet the goals of ${company_name}.
 `;
   console.error(job_profile);
   return await query(professional_summary_prompt, 4);
