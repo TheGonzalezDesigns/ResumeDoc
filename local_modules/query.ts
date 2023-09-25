b5dfeaf68dd30ae26138cdd6ff137afa38ea0d23
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-24 22:36:54 -0700

    Refactor query function to use model enums  Co-authored-by: Assistant

--- local_modules/query.ts
+++ local_modules/query.ts
@@ -5,24 +5,30 @@ enum GPT {
   GPT4 = 4,
 }
 
+// Define available models
 const models = {
   3: "gpt-3.5-turbo",
   4: "gpt-4",
 };
 
+/**
+ * Queries an OpenAI model and returns the generated response.
+ *
+ * @param {string} prompt - The prompt to be sent to the OpenAI model.
+ * @param {GPT} modelType - The type of GPT model to be used (default is GPT3).
+ * @returns {Promise<string>} A promise that resolves to the generated response from the OpenAI model.
+ */
 export const query = async (
   prompt: string,
-  modelType: GPT = 3
+  modelType: GPT = GPT.GPT3
 ): Promise<string> => {
+  // Initialize the OpenAI model
   const model = new OpenAI({
     modelName: models[modelType],
     temperature: 0.9,
   });
 
+  // Query the model and return the response
   const res = await model.call(prompt);
-  console.info({
-    query: prompt,
-    reply: res,
-  });
   return res;
 };
