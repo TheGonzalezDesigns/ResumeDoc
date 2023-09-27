8f430cf87976e84d739ccaf53e2f0dcade3acaa8
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-26 23:49:42 -0700

    Update OpenAI model naming, initialize models, and add a random hash generator function Refactor OpenAI model naming and initialization, and add a random hash generator function

--- local_modules/query.ts
+++ local_modules/query.ts
@@ -6,29 +6,56 @@ enum GPT {
 }
 
 // Define available models
-const models = {
+const model_names = {
   3: "gpt-3.5-turbo",
   4: "gpt-3.5-turbo-16k-0613",
+};
+
+const temperature = 0.1;
+
+// Pre-initialize OpenAI models
+const models = {
+  3: new OpenAI({
+    modelName: model_names[3],
+    temperature,
+  }),
+  4: new OpenAI({
+    modelName: model_names[4],
+    temperature,
+  }),
+};
+
+/**
+ * Generates a random hash for timing queries.
+ * @returns {string} A random hash string.
+ */
+const random_hash_generator = (): string => {
+  let hash = "";
+  const possible_chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 10; i++) {
+    hash += possible_chars.charAt(
+      Math.floor(Math.random() * possible_chars.length)
+    );
+  }
+
+  return hash;
 };
 
 /**
  * Queries an OpenAI model and returns the generated response.
  *
  * @param {string} prompt - The prompt to be sent to the OpenAI model.
- * @param {GPT} modelType - The type of GPT model to be used (default is GPT3).
+ * @param {GPT} model_type - The type of GPT model to be used (default is GPT3).
  * @returns {Promise<string>} A promise that resolves to the generated response from the OpenAI model.
  */
 export const query = async (
   prompt: string,
-  modelType: GPT = GPT.GPT3
+  model_type: GPT = GPT.GPT3
 ): Promise<string> => {
-  // Initialize the OpenAI model
-  const model = new OpenAI({
-    modelName: models[modelType],
-    temperature: 0.9,
-  });
-  console.warn("Query:", prompt);
-  // Query the model and return the response
+  const timer = `[[querying-ai]]-${random_hash_generator()}`;
+  const model = models[model_type];
   const res = await model.call(prompt);
   return res;
 };
