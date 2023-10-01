b63de456cfec69b6a677c6dd40c72fb58f846791
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-01 10:21:05 -0700

    Updated server.ts file with changes to the 'status' property

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ app.post("/generate", async (c) => {
       resume: "",
       cover_letter: "",
     },
-    status: false,
+    status: true,
   };
 
   console.info("job_profile:", job_profile);
@@ -60,8 +60,69 @@ app.post("/generate", async (c) => {
   } catch (error) {
     console.error("Generation failed:", error);
   }
-  //// Replace ... with your logic
   return c.json({ log });
+});
+
+app.post("/simulate", async (c) => {
+  try {
+    const commands = (await c.req.json())?.commands;
+
+    if (!Array.isArray(commands)) {
+      console.error("Invalid command format: commands should be an array");
+      return c.text("Invalid command format", 400);
+    }
+
+    for (const command of commands) {
+      if (
+        typeof command.type !== "string" ||
+        !["key", "keys"].includes(command.type)
+      ) {
+        console.error("Invalid command type:", command.type);
+        return c.text("Invalid command type", 400);
+      }
+
+      try {
+        let child;
+        if (command.type === "key" && typeof command.key === "string") {
+          child = Bun.spawn(["xdotool", "key", command.key]);
+        } else if (
+          command.type === "keys" &&
+          typeof command.keys === "string"
+        ) {
+          child = Bun.spawn(["xdotool", "type", command.keys]);
+        } else {
+          console.error("Invalid command format:", command);
+          return c.text("Invalid command format", 400);
+        }
+
+        if (child) {
+          const stdoutText = await new Response(child.stdout).text();
+          console.log("STDOUT from child process:", stdoutText);
+
+          if (child.stderr) {
+            const stderrText = await new Response(child.stderr).text();
+            console.error("STDERR from child process:", stderrText);
+          }
+
+          child.exited
+            .then((exitCode) => {
+              console.log(`Child process exited with code: ${exitCode}`);
+            })
+            .catch((error) => {
+              console.error("Child process exited with error:", error);
+            });
+        }
+      } catch (error) {
+        console.error("Error executing command:", command, error);
+        return c.text("Error executing command", 500);
+      }
+    }
+
+    return c.json({ status: "success" });
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    return c.text("Unexpected Error", 500);
+  }
 });
 
 export default {
