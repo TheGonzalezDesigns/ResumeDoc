4d53d4b2423535528e4223a6f7678d35d6bf85ed
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-04 13:39:30 -0700

    Update questionnaire_surgeon.ts with personal_data prompt

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -1,14 +1,19 @@
 import { query } from "./query";
 
+const { basic_info } = await Bun.file("./context/personal/profile.json").json();
+const personal_data = JSON.stringify(basic_info);
 export type Script = string | string[];
 
 export const questionnaire_surgeon = async (
   html_snippet: string
 ): Promise<Script> => {
+  const prompt = `
+  basic_info: ${personal_data}
+  `;
   const script = `
   console.clear();
   console.info('Hey good looking');
   `;
 
-  return await script;
+  return await prompt;
 };
