5411da51f4c0c3c3461c47ce96eb4b927868e4c9
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-04 15:01:17 -0700

    Modify questionnaire_surgeon.ts to include personal_summary in the function arguments and add a prompt string that includes personal_summary and instructions for answering questions in an HTML snippet for a job application. The function will generate a script based on the prompt and return it.

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -1,19 +1,27 @@
 import { query } from "./query";
 
-const { basic_info } = await Bun.file("./context/personal/profile.json").json();
-const personal_data = JSON.stringify(basic_info);
 export type Script = string | string[];
 
+const { basic_info } = await Bun.file("./context/personal/profile.json").json();
+const personal_data = JSON.stringify(basic_info);
+const instructions = `The following is a snippet of html containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this html snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
+const system_prompt = `You are an expert in extracting queries from html, answering them according to the info below, and generating code that when executed will fill in the html forms accordingly.`;
+
 export const questionnaire_surgeon = async (
-  html_snippet: string
+  html_snippet: string,
+  personal_summary: string
 ): Promise<Script> => {
   const prompt = `
+  System: ${system_prompt}
   basic_info: ${personal_data}
-  `;
-  const script = `
-  console.clear();
-  console.info('Hey good looking');
+  personal_summary: ${personal_summary}
+
+  Instructions: ${instructions}
+
+  html_snippet: ${html_snippet}
   `;
 
-  return await prompt;
+  const script = await query(prompt);
+
+  return script;
 };
