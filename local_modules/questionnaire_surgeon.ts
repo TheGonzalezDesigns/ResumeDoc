333b48fed55de0b50d932c3aa55e59227e2ebf0d
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-05 08:06:03 -0700

    Update questionnaire_surgeon.ts to handle HTML snippets for job application questions.

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -3,20 +3,29 @@ import { HTML_assistant, HTML_Type } from "./HTML_assistant";
 
 export type Script = string | string[];
 
+/**
+ * Retrieve basic and legal information from a JSON file.
+ */
 const { basic_info, legal_info } = await Bun.file(
   "./context/personal/profile.json"
 ).json();
 const basic_data = JSON.stringify(basic_info);
 const legal_data = JSON.stringify(legal_info);
-const instructions = `The following is a snippet of HTML containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this HTML snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
-const system_prompt = `You are an expert in extracting queries from HTML, answering them according to the info below, and generating code that when executed will fill in the HTML forms accordingly.`;
 
+const instructions = `The following is a snippet of HTML containing questions for a job application...`;
+const system_prompt = `You are an expert in extracting queries from HTML, answering them according to the info below...`;
+
+/**
+ * Main function to process HTML snippet and generate JavaScript code to interact with it.
+ * @param {string} HTML_snippet - HTML content as a string.
+ * @param {string} personal_summary - Personal summary of the user.
+ * @returns {Promise<Script>} - JavaScript code as a string or array of strings.
+ */
 export const questionnaire_surgeon = async (
   HTML_snippet: string,
   personal_summary: string
 ): Promise<Script> => {
   const { types } = HTML_assistant(HTML_snippet);
-
   const generated_data = types.includes(HTML_Type.Generated)
     ? `Date Available: ${get_upcoming_monday()}`
     : "";
@@ -32,19 +41,14 @@ export const questionnaire_surgeon = async (
   System: ${system_prompt}
   basic_info: ${basic_data}
   personal_summary: ${personal_summary}
-
   Instructions: ${instructions}
-
   HTML_snippet: ${HTML_snippet}
   `;
 
   const response = await query(prompt, 4);
 
   const reprompt = `
-  Please extract the code from the following text and place it into this json object, as the value for the 'code' key, only return the JSON object:
-  {
-    code: ""
-  }
+  Please extract the code from the following text...
   text: ${response}
   `;
 
@@ -55,13 +59,21 @@ export const questionnaire_surgeon = async (
   return script;
 };
 
+/**
+ * Generate a prompt string based on the HTML types present.
+ * @param {HTML_Type[]} types - Types of HTML elements.
+ * @param {string} basic_data - Basic information as JSON string.
+ * @param {string} legal_data - Legal information as JSON string.
+ * @param {string} generated_data - Generated data like upcoming Monday date.
+ * @returns {string} - Assembled prompt string.
+ */
 const get_relevant_data_prompt = (
   types: HTML_Type[],
   basic_data: string,
   legal_data: string,
   generated_data: string
 ): string => {
-  let relevant_prompt_data: string[] = [];
+  const relevant_prompt_data: string[] = [];
 
   if (types.includes(HTML_Type.Basic)) {
     relevant_prompt_data.push(`basic_info: ${basic_data}`);
@@ -73,10 +85,13 @@ const get_relevant_data_prompt = (
     relevant_prompt_data.push(`generated_info: ${generated_data}`);
   }
 
-  // Combine all data strings into one
   return relevant_prompt_data.join("\n  ");
 };
 
+/**
+ * Calculate the upcoming Monday date.
+ * @returns {string} - The upcoming Monday date in MM/DD/YYYY format.
+ */
 const get_upcoming_monday = (): string => {
   const today = new Date();
   const dayOfWeek = today.getDay();
