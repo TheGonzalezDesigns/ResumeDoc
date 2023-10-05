4cefa0a9ea7bddcb6deae90dca7ff417152de6f9
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-05 07:45:22 -0700

    Generate a nuanced commit message Refactor questionnaire_surgeon.ts to improve code readability and maintainability.

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -1,4 +1,5 @@
 import { query } from "./query";
+import { HTML_assistant, HTML_Type } from "./HTML_assistant";
 
 export type Script = string | string[];
 
@@ -7,13 +8,26 @@ const { basic_info, legal_info } = await Bun.file(
 ).json();
 const basic_data = JSON.stringify(basic_info);
 const legal_data = JSON.stringify(legal_info);
-const instructions = `The following is a snippet of html containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this html snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
-const system_prompt = `You are an expert in extracting queries from html, answering them according to the info below, and generating code that when executed will fill in the html forms accordingly.`;
+const instructions = `The following is a snippet of HTML containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this HTML snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
+const system_prompt = `You are an expert in extracting queries from HTML, answering them according to the info below, and generating code that when executed will fill in the HTML forms accordingly.`;
 
 export const questionnaire_surgeon = async (
-  html_snippet: string,
+  HTML_snippet: string,
   personal_summary: string
 ): Promise<Script> => {
+  const { types } = HTML_assistant(HTML_snippet);
+
+  const generated_data = types.includes(HTML_Type.Generated)
+    ? `Date Available: ${get_upcoming_monday()}`
+    : "";
+
+  const relevant_data_prompt = get_relevant_data_prompt(
+    types,
+    basic_data,
+    legal_data,
+    generated_data
+  );
+
   const prompt = `
   System: ${system_prompt}
   basic_info: ${basic_data}
@@ -21,7 +35,7 @@ export const questionnaire_surgeon = async (
 
   Instructions: ${instructions}
 
-  html_snippet: ${html_snippet}
+  HTML_snippet: ${HTML_snippet}
   `;
 
   const response = await query(prompt, 4);
@@ -41,7 +55,29 @@ export const questionnaire_surgeon = async (
   return script;
 };
 
-const getUpcomingMonday = (): string => {
+const get_relevant_data_prompt = (
+  types: HTML_Type[],
+  basic_data: string,
+  legal_data: string,
+  generated_data: string
+): string => {
+  let relevant_prompt_data: string[] = [];
+
+  if (types.includes(HTML_Type.Basic)) {
+    relevant_prompt_data.push(`basic_info: ${basic_data}`);
+  }
+  if (types.includes(HTML_Type.Legal)) {
+    relevant_prompt_data.push(`legal_info: ${legal_data}`);
+  }
+  if (types.includes(HTML_Type.Generated)) {
+    relevant_prompt_data.push(`generated_info: ${generated_data}`);
+  }
+
+  // Combine all data strings into one
+  return relevant_prompt_data.join("\n  ");
+};
+
+const get_upcoming_monday = (): string => {
   const today = new Date();
   const dayOfWeek = today.getDay();
   const daysUntilMonday = (7 - dayOfWeek) % 7;
