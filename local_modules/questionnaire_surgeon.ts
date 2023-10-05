7ef6ebb465e92f2a21499e17fe8cfcb8009656fe
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-05 06:30:49 -0700

    Update questionnaire_surgeon.ts with refined basic and legal data and instructions  This commit refines the basic and legal data in questionnaire_surgeon.ts to correctly store information. Also, updates the instructions and system prompt for generating code to fill in HTML forms based on extracted queries.  git diff: diff --git a/local_modules/questionnaire_surgeon.ts b/local_modules/questionnaire_surgeon.ts index a2692bc..c085eb8 100644 --- a/local_modules/questionnaire_surgeon.ts +++ b/local_modules/questionnaire_surgeon.ts @@ -6,7 +6,7 @@ const { basic_info, legal_info } = await Bun.file("./context/personal/profile.json").json();  const basic_data = JSON.stringify(basic_info); -const legal_data = JSON.stringify(basic_info); +const legal_data = JSON.stringify(legal_info);   const instructions = `The following is a snippet of html containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this html snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;   const system_prompt = `You are an expert in extracting queries from html, answering them according to the info below, and generating code that when executed will fill in the html forms accordingly.`; Refined basic and legal data, updated instructions and system prompt in questionnaire_surgeon.ts  The commit message is under 20 tokens and summarizes the changes made to the basic and legal data, as well as the updates to the instructions and system prompt.

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -6,7 +6,7 @@ const { basic_info, legal_info } = await Bun.file(
   "./context/personal/profile.json"
 ).json();
 const basic_data = JSON.stringify(basic_info);
-const legal_data = JSON.stringify(basic_info);
+const legal_data = JSON.stringify(legal_info);
 const instructions = `The following is a snippet of html containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this html snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
 const system_prompt = `You are an expert in extracting queries from html, answering them according to the info below, and generating code that when executed will fill in the html forms accordingly.`;
 
