6a9c6893ba4920b6358d1c9b8accb3eb56cb4460
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-05 07:24:50 -0700

    Refactor HTML_assistant.ts: Update report type to include multiple HTML types Refactor HTML_assistant.ts: Updated report type to include multiple HTML types

--- local_modules/HTML_assistant.ts
+++ local_modules/HTML_assistant.ts
@@ -1,21 +1,21 @@
-enum HTML_Type {
-  Legal = 0,
-  Personal = 1,
-  Basic = 2,
-  Mixed = 3,
-}
-
 type HTML = string;
 
 export interface Report {
   HTML_snippet: HTML;
-  type: HTML_Type;
+  types: HTML_Type[];
+}
+
+export enum HTML_Type {
+  Legal = 0,
+  Personal = 1,
+  Basic = 2,
+  Generated = 3,
 }
 
 export const HTML_assistant = (HTML_snippet: HTML): Report => {
   let report: Report = {
     HTML_snippet,
-    type: HTML_Type.Mixed,
+    types: [],
   };
 
   const legalPattern = /<input type="(?:radio|checkbox)">/g;
@@ -23,19 +23,17 @@ export const HTML_assistant = (HTML_snippet: HTML): Report => {
   const personalPattern = /<input type="text">/g;
   const generatedPattern = /<input type="(?:week|month|date|datetime-local)">/g;
 
-  const isLegal = legalPattern.test(HTML_snippet);
-  const isBasic = basicPattern.test(HTML_snippet);
-  const isPersonal = personalPattern.test(HTML_snippet);
-  const isGenerated = generatedPattern.test(HTML_snippet);
-
-  if (isLegal && !isBasic && !isPersonal && !isGenerated) {
-    report.type = HTML_Type.Legal;
-  } else if (!isLegal && isBasic && !isPersonal && !isGenerated) {
-    report.type = HTML_Type.Basic;
-  } else if (!isLegal && !isBasic && isPersonal && !isGenerated) {
-    report.type = HTML_Type.Personal;
-  } else {
-    report.type = HTML_Type.Mixed;
+  if (legalPattern.test(HTML_snippet)) {
+    report.types.push(HTML_Type.Legal);
+  }
+  if (basicPattern.test(HTML_snippet)) {
+    report.types.push(HTML_Type.Basic);
+  }
+  if (personalPattern.test(HTML_snippet)) {
+    report.types.push(HTML_Type.Personal);
+  }
+  if (generatedPattern.test(HTML_snippet)) {
+    report.types.push(HTML_Type.Generated);
   }
 
   return report;
