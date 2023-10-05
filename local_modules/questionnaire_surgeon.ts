a24ac461e6acdcd5cd16d0fb5b1864b259865902
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-10-05 06:28:54 -0700

    Refactor "questionnaire_surgeon.ts" and add "getUpcomingMonday" function Refactor "questionnaire_surgeon.ts" and add "getUpcomingMonday" function

--- local_modules/questionnaire_surgeon.ts
+++ local_modules/questionnaire_surgeon.ts
@@ -2,7 +2,9 @@ import { query } from "./query";
 
 export type Script = string | string[];
 
-const { basic_info } = await Bun.file("./context/personal/profile.json").json();
+const { basic_info, legal_info } = await Bun.file(
+  "./context/personal/profile.json"
+).json();
 const personal_data = JSON.stringify(basic_info);
 const instructions = `The following is a snippet of html containing questions for a job application. This is a test, you can awnser in any way. Your job is to identify the questions, awnser them, then identify what kind of form each question is, so you can either type or select the appropriate response or option[s]; when you respond you have to come up with javascript code that leverages querSelectors to click or insert the correct response. The code you return should be executable and working if pasted into the console log of the page where this html snippet originated. For any form that is a button, radio button or not a text field, please use .click() instead of .checked = true. Only return the code and nothing else.`;
 const system_prompt = `You are an expert in extracting queries from html, answering them according to the info below, and generating code that when executed will fill in the html forms accordingly.`;
@@ -36,4 +38,18 @@ export const questionnaire_surgeon = async (
   const { code: script } = JSON.parse(formatted_response);
 
   return script;
+};
+
+const getUpcomingMonday = (): string => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysUntilMonday = (7 - dayOfWeek) % 7;
+  const upcomingMonday = new Date(
+    today.getTime() + daysUntilMonday * 24 * 60 * 60 * 1000
+  );
+  const month = upcomingMonday.getMonth() + 1;
+  const day = upcomingMonday.getDate();
+  const year = upcomingMonday.getFullYear();
+
+  return `${month}/${day}/${year}`;
 };
